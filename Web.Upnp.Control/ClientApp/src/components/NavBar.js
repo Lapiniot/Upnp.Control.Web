622c09f9bfc11faa2e28c838c5cad71832611a59
Lapiniot <aossss@gmail.com>
2018-06-11 00:39:56 +0300

    icons in navbar

--- Web.Upnp.Control/ClientApp/src/components/NavBar.js
+++ Web.Upnp.Control/ClientApp/src/components/NavBar.js
@@ -9,10 +9,10 @@ export class NavBar extends Component {
             <nav class="navbar navbar-dark bg-dark flex-column justify-content-start align-items-stretch">
                 <div class="navbar-brand">UPnP Controller</div>
                 <ul class="navbar-nav">
-                    <li class="nav-item fa fa-home"><Link to={'/'} class="nav-link">Home</Link></li>
-                    <li class="nav-item fa fa-music"><Link to={'/umi'} class="nav-link">Network Speakers</Link></li>
-                    <li class="nav-item fa fa-server"><Link to={'/upnp'} class="nav-link">UPnP devices</Link></li>
-                    <li class="nav-item fa fa-cogs"><Link to={'/settings'} class="nav-link">Settings</Link></li>
+                    <li class="nav-item"><i class="fa fa-home" /><Link to={'/'} class="nav-link">Home</Link></li>
+                    <li class="nav-item"><i class="fa fa-music" /><Link to={'/umi'} class="nav-link">Network Speakers</Link></li>
+                    <li class="nav-item"><i class="fa fa-server" /><Link to={'/upnp'} class="nav-link">UPnP devices</Link></li>
+                    <li class="nav-item"><i class="fa fa-cogs" /><Link to={'/settings'} class="nav-link">Settings</Link></li>
                 </ul>
             </nav>
         );
