3655d780b40288686d2306f25b21074454403a7b
Lapiniot <aossss@gmail.com>
2018-06-18 16:02:12 +0300

    code cleanup

--- Web.Upnp.Control/Controllers/DiscoveryController.cs
+++ Web.Upnp.Control/Controllers/DiscoveryController.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IoT.Device.Upnp;
 using IoT.Protocol;
 using IoT.Protocol.Upnp;
 using Microsoft.AspNetCore.Mvc;
-using Web.Upnp.Control.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,31 +18,30 @@ namespace Web.Upnp.Control.Controllers
         [HttpGet]
         public async Task<IEnumerable<object>> Get()
         {
-            List<Task<UpnpDeviceDescription>> tasks = new List<Task<UpnpDeviceDescription>>();
+            var tasks = new List<Task<UpnpDeviceDescription>>();
 
-            var enumerator = new IoT.Device.Upnp.UpnpDeviceEnumerator(UpnpServices.RootDevice);
+            var enumerator = new UpnpDeviceEnumerator(UpnpServices.RootDevice);
 
             foreach(var d in enumerator.Enumerate(TimeSpan.FromSeconds(5)))
             {
                 tasks.Add(d.GetDescriptionAsync());
             }
 
-            await Task.WhenAll(tasks);
+            await Task.WhenAll(tasks).ConfigureAwait(false);
 
-            return tasks.Where(t => t.IsCompletedSuccessfully).
-                Select(t => new
-                {
-                    Udn = t.Result.Udn,
-                    Url = t.Result.Location,
-                    DeviceType = t.Result.DeviceType,
-                    Name = t.Result.FriendlyName,
-                    Manufacturer = t.Result.Manufacturer,
-                    Description = t.Result.ModelDescription,
-                    ModelName = t.Result.ModelName,
-                    ModelNumber = t.Result.ModelNumber,
-                    Icons = t.Result.Icons.Select(i => new { W = i.Width, H = i.Height, Url = i.Uri }),
-                    Services = t.Result.Services.Select(s => new { Id = s.ServiceId, url = s.MetadataUri }).ToArray()
-                });
+            return tasks.Where(t => t.IsCompletedSuccessfully).Select(t => new
+            {
+                t.Result.Udn,
+                Url = t.Result.Location,
+                t.Result.DeviceType,
+                Name = t.Result.FriendlyName,
+                t.Result.Manufacturer,
+                Description = t.Result.ModelDescription,
+                t.Result.ModelName,
+                t.Result.ModelNumber,
+                Icons = t.Result.Icons.Select(i => new {W = i.Width, H = i.Height, Url = i.Uri}),
+                Services = t.Result.Services.Select(s => new {Id = s.ServiceId, Url = s.MetadataUri}).ToArray()
+            });
         }
 
         // GET api/<controller>/5
