ce989506cca6a10b6a033094c7a9f53ba5b563c5
Lapiniot <aossss@gmail.com>
2018-07-08 23:45:49 +0300

    Pagination support in the ContentBrowser

--- Web.Upnp.Control/ClientApp/src/components/upnp/ContentBrowser.js
+++ Web.Upnp.Control/ClientApp/src/components/upnp/ContentBrowser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DataView from "../DataView";
-import { withRouter, NavLink } from "react-router-dom"
+import { withRouter, NavLink } from "react-router-dom";
+import { QString } from "../Utils";
 
 function getKind(upnpClassName) {
     let index = upnpClassName.lastIndexOf(".");
@@ -45,7 +46,49 @@ class Breadcrumbs extends React.Component {
 
 class Pagination extends React.Component {
     render() {
-        return <div></div>;
+        const { "data-context": { result: { length } = [], total = 0 } = {}, baseUrl, page, size } = this.props;
+
+        if (length === 0 || total === length) return null;
+
+        let pageData = [];
+        for (var i = 0; i < Math.ceil(total / size); i++) {
+            pageData.push({ title: i + 1, url: `${baseUrl}?p=${i + 1}&s=${size}` });
+        }
+
+        const isFirst = page == 1;
+        const isLast = page >= pageData.length;
+        
+        return <nav aria-label="Page navigation example">
+            <ul className="pagination justify-content-center">
+                <li className={`page-item${isFirst ? " disabled" : ""}`}>{!isFirst ?
+                    <NavLink to={`${baseUrl}?p=${page - 1}&s=${size}`} className="page-link" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span><span className="sr-only">Previous</span>
+                    </NavLink> :
+                    <span className="page-link disabled" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </span>}
+                </li>
+                {[
+                    pageData.map((e, index) => {
+                        const isCurrent = index + 1 === page;
+
+                        return <li key={index} className={`page-item${isCurrent ? " active" : ""}`}>
+                            {isCurrent ?
+                                <span className="page-link">{e.title}<span class="sr-only">(current)</span></span> :
+                                <NavLink to={e.url} className="page-link">{e.title}</NavLink>}
+                        </li>;
+                    })
+                ]}
+                <li className={`page-item${isLast ? " disabled" : ""}`}>{!isLast ?
+                    <NavLink to={`${baseUrl}?p=${page + 1}&s=${size}`} className="page-link" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span><span className="sr-only">Next</span>
+                    </NavLink> :
+                    <span className="page-link" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </span>}
+                </li>
+            </ul>
+        </nav>;
     }
 }
 
@@ -83,7 +126,7 @@ class ContainerView extends React.Component {
             </div>
             <div>
                 {(parents && parents.length > 0) &&
-                    <div onDoubleClick={() => navigateHandler(parents[parents.length - 1].parentId)}>
+                    <div data-id={parents[parents.length - 1].parentId} onDoubleClick={navigateHandler}>
                         <div>...</div>
                         <div>Parent</div>
                     </div>}
@@ -95,7 +138,7 @@ class ContainerView extends React.Component {
 
 class Browser extends React.Component {
 
-    handleDoubleClick = event => {
+    navigateHandler = event => {
         const id = event.currentTarget.dataset.id;
         this.navigateToItem(id);
     }
@@ -112,13 +155,14 @@ class Browser extends React.Component {
     }
 
     render() {
-        const { device, id, baseUrl } = this.props;
-        return <DataView dataUri={`/api/browse/${device}/${id}?withParents=true`}
+        const { device, id, baseUrl, match: { url }, location: { search: qstring } } = this.props;
+        const { p: page = 1, s: size = 50 } = QString.parse(qstring);
+        return <DataView dataUri={`/api/browse/${device}/${id}?withParents=true&take=${size}&skip=${(page - 1) * size}`}
             selector={data => data.result}
             headerTemplate={Breadcrumbs} headerProps={{ baseUrl: `${baseUrl}/${device}` }}
-            containerTemplate={ContainerView} containerProps={{ navigateHandler: this.navigateToItem }}
-            itemTemplate={DIDLItem} itemProps={{ onDoubleClick: this.handleDoubleClick }}
-            footerTemplate={Pagination} />;
+            containerTemplate={ContainerView} containerProps={{ navigateHandler: this.navigateHandler }}
+            itemTemplate={DIDLItem} itemProps={{ onDoubleClick: this.navigateHandler }}
+            footerTemplate={Pagination} footerProps={{ baseUrl: url, page: parseInt(page), size: parseInt(size) }} />;
     }
 }
 
