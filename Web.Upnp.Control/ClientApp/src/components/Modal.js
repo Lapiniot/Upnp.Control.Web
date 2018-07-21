c2a61e7dc96220fd938992d3a4550e41fc73180d
Lapiniot <aossss@gmail.com>
2018-07-21 19:59:16 +0300

    Modal.Button defined as static property

--- Web.Upnp.Control/ClientApp/src/components/Modal.js
+++ Web.Upnp.Control/ClientApp/src/components/Modal.js
@@ -25,33 +25,33 @@ export default class Modal extends React.Component {
         const { id, immidiate, area, title, area: { label: areaLabel, hidden: areaHidden = true } = {}, ...other } = this.props;
 
         return <React.Fragment>
-                   <div className="modal fade" id={id} tabIndex="-1" role="dialog" aria-labelledby={areaLabel} aria-hidden={areaHidden} {...other}>
-                       <div className="modal-dialog modal-dialog-centered" role="document">
-                           <div className="modal-content">
-                               <div className="modal-header">
-                                   <h5 className="modal-title" id="exampleModalCenterTitle">{title}</h5>
-                                   <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                       <span aria-hidden="true">&times;</span>
-                                   </button>
-                               </div>
-                               <div className="modal-body">
-                                   {typeof this.props.renderBody === "function" ? this.props.renderBody() : this.props.children}
-                               </div>
-                               <div className="modal-footer">
-                                   {typeof this.props.renderFooter === "function"
-                                       ? this.props.renderFooter()
-                                       : [this.props.buttons]}
-                               </div>
-                           </div>
-                       </div>
-                   </div>
-               </React.Fragment>;
+            <div className="modal fade" id={id} tabIndex="-1" role="dialog" aria-labelledby={areaLabel} aria-hidden={areaHidden} {...other}>
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalCenterTitle">{title}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {typeof this.props.renderBody === "function" ? this.props.renderBody() : this.props.children}
+                        </div>
+                        <div className="modal-footer">
+                            {typeof this.props.renderFooter === "function"
+                                ? this.props.renderFooter()
+                                : [this.props.buttons]}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>;
     }
-}
 
-Modal.Button = class extends React.Component {
-    render() {
-        const { dismiss, text, ...other } = this.props;
-        return <button type="button" className="btn" data-dismiss={dismiss ? "modal" : null} {...other}>{text}</button>;
-    }
+    static Button = class extends React.Component {
+        render() {
+            const { dismiss, text, ...other } = this.props;
+            return <button type="button" className="btn" data-dismiss={dismiss ? "modal" : null} {...other}>{text}</button>;
+        }
+    };
 };
