2822d2ba82db1dc39cb13e2233a31a7dda404a9b
Lapiniot <aossss@gmail.com>
2018-07-28 19:47:09 +0300

    Discovery API: 1. media_servers & media_renderers categories added 2. General controller refactoring and optimization

--- Web.Upnp.Control/Controllers/DiscoveryController.cs
+++ Web.Upnp.Control/Controllers/DiscoveryController.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using IoT.Device.Xiaomi.Umi.Services;
+using IoT.Protocol.Upnp;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Web.Upnp.Control.DataAccess;
+using Web.Upnp.Control.Models.Database.Upnp;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Web.Upnp.Control.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/{filter?}")]
     public class DiscoveryController : Controller
     {
+        private static IDictionary<string, Expression<Func<Device, bool>>> filters = new Dictionary<string, Expression<Func<Device, bool>>>()
+        {
+            {"umi", d => d.Services.Any(s => s.ServiceType == PlaylistService.ServiceSchema)},
+            {"upnp", d => true},
+            {"media_servers", d => d.DeviceType == UpnpServices.MediaServer ||
+                d.Services.Any(s => s.ServiceType == UpnpServices.ContentDirectory ||
+                    s.ServiceType == PlaylistService.ServiceSchema)},
+            {"media_renderers",d => d.DeviceType == UpnpServices.MediaRenderer ||
+                    d.Services.Any(s => s.ServiceType == UpnpServices.MediaRenderer)}
+        };
+
         private readonly UpnpDbContext context;
 
         public DiscoveryController(UpnpDbContext context)
@@ -21,43 +36,18 @@ namespace Web.Upnp.Control.Controllers
         }
 
         [HttpGet]
-        public Task<IEnumerable<object>> GetAsync()
+        public Task<Device[]> GetAsync(string filter = "upnp")
         {
-            return GetUpnpDevicesAsync();
+            return filters.TryGetValue(filter, out var filterExpression) ? QueryAsync(filterExpression) :
+                throw new ArgumentException("Unknown device category filter '" + filter + "'");
         }
 
-        [HttpGet("{type}")]
-        public Task<IEnumerable<object>> GetAsync(string type)
+        private Task<Device[]> QueryAsync(Expression<Func<Device, bool>> filter)
         {
-            switch(type)
-            {
-                case "umi":
-                    return GetUmiDevicesAsync();
-                case "upnp":
-                    return GetUpnpDevicesAsync();
-                default:
-                    throw new ArgumentException("Invalid value", nameof(type));
-            }
-        }
-
-        private async Task<IEnumerable<object>> GetUpnpDevicesAsync()
-        {
-            return await context.UpnpDevices
-                .Include(d => d.Icons)
-                .Include(d => d.Services)
-                .Where(d => d.IsOnline)
-                .ToArrayAsync()
-                .ConfigureAwait(false);
-        }
-
-        private async Task<IEnumerable<object>> GetUmiDevicesAsync()
-        {
-            return await context.UpnpDevices
-                .Include(d => d.Icons)
-                .Include(d => d.Services)
-                .Where(d => d.IsOnline && d.Description == "The Mi WiFi SoundBox")
-                .ToArrayAsync()
-                .ConfigureAwait(false);
+            return context.UpnpDevices
+                .Include(d => d.Icons).Include(d => d.Services)
+                .Where(d => d.IsOnline).Where(filter)
+                .ToArrayAsync();
         }
     }
 }
