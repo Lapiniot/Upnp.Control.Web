ded1a4fd6be7d1484caa57eca8af5d0f7874b6ad
Lapiniot <aossss@gmail.com>
2018-08-02 13:32:13 +0300

    HttpSoapClient and ImageLoaderProxyClient typed clients introduced

---
+++ Web.Upnp.Control/Services/HttpClients/HttpSoapClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+
+namespace Web.Upnp.Control.Services.HttpClients
+{
+    public class HttpSoapClient
+    {
+        public HttpSoapClient(HttpClient client)
+        {
+            Client = client;
+            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip,deflate");
+        }
+
+        public HttpClient Client { get; }
+    }
+}
---
+++ Web.Upnp.Control/Services/HttpClients/ImageLoaderClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Web.Upnp.Control.Services.HttpClients
+{
+    public class ImageLoaderProxyClient
+    {
+        private readonly HttpClient client;
+
+        public ImageLoaderProxyClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string originalUri, IHeaderDictionary originalHeaders, CancellationToken cancellationToken = default)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Get, originalUri);
+
+            foreach(var h in originalHeaders)
+            {
+                if(h.Key == "Host") continue;
+
+                message.Headers.TryAddWithoutValidation(h.Key, (IEnumerable<string>)h.Value);
+            }
+
+            return client.SendAsync(message, cancellationToken);
+        }
+    }
+}
