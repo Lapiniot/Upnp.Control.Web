f70d20e1424d15f9c7ace396acd90e11f939efda
Lapiniot <aossss@gmail.com>
2018-12-31 19:09:39 +0200

    UpnpDiscoveryService redesigned

--- Web.Upnp.Control/Services/UpnpDiscoveryService.cs
+++ Web.Upnp.Control/Services/UpnpDiscoveryService.cs
@@ -11,8 +11,11 @@ using static IoT.Protocol.Upnp.UpnpServices;
 
 namespace Web.Upnp.Control.Services
 {
-    public class UpnpDiscoveryService : IHostedService
+    public class UpnpDiscoveryService : IHostedService, IDisposable
     {
+        private Task monitorTask;
+        private CancellationTokenSource tcs;
+
         public UpnpDiscoveryService(IServiceProvider services)
         {
             Services = services;
@@ -20,63 +23,77 @@ namespace Web.Upnp.Control.Services
 
         public IServiceProvider Services { get; }
 
+        #region Implementation of IDisposable
+
+        public void Dispose() {}
+
+        #endregion
+
         #region Implementation of IHostedService
 
-        public async Task StartAsync(CancellationToken cancellationToken)
+        public Task StartAsync(CancellationToken cancellationToken)
         {
-            using(var tcs = new CancellationTokenSource(10000))
-            {
-                var enumerator = new UpnpDeviceEnumerator(TimeSpan.FromSeconds(5), RootDevice);
-                await enumerator.DiscoverAsync(OnDiscovered, tcs.Token).ConfigureAwait(false);
-            }
-        }
-
-        private async void OnDiscovered(UpnpDevice device)
-        {
-            using(var scoped = Services.CreateScope())
-            {
-                using(var context = scoped.ServiceProvider.GetRequiredService<UpnpDbContext>())
-                {
-                    // TODO: configure and start UPnP multi-cast listener in order to track device lifetime events
-                    var description = await device.GetDescriptionAsync();
-
-                    var entity = new Device
-                    {
-                        Udn = description.Udn,
-                        Location = description.Location.AbsoluteUri,
-                        DeviceType = description.DeviceType,
-                        FriendlyName = description.FriendlyName,
-                        Manufacturer = description.Manufacturer,
-                        Description = description.ModelDescription,
-                        ModelName = description.ModelName,
-                        ModelNumber = description.ModelNumber,
-                        IsOnline = true,
-                        Icons = description.Icons.Select(i => new Icon
-                        {
-                            Width = i.Width,
-                            Height = i.Height,
-                            Mime = i.Mime,
-                            Url = i.Uri.AbsoluteUri
-                        }).ToList(),
-                        Services = description.Services.Select(s => new Service
-                        {
-                            ServiceId = s.ServiceId,
-                            ServiceType = s.ServiceType,
-                            MetadataUrl = s.MetadataUri.AbsoluteUri,
-                            ControlUrl = s.ControlUri.AbsoluteUri
-                        }).ToList()
-                    };
-
-                    await context.AddAsync(entity).ConfigureAwait(false);
-
-                    await context.SaveChangesAsync().ConfigureAwait(false);
-                }
-            }
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
+            tcs = new CancellationTokenSource();
+            monitorTask = StartMonitorAsync(tcs.Token);
             return Task.CompletedTask;
+        }
+
+        private async Task StartMonitorAsync(CancellationToken cancellationToken)
+        {
+            var enumerator = new UpnpDeviceEnumerator(TimeSpan.FromSeconds(30), RootDevice);
+
+            using(var scope = Services.CreateScope())
+            using(var context = scope.ServiceProvider.GetRequiredService<UpnpDbContext>())
+            {
+                await enumerator.DiscoverAsync(device => DiscoveredAsync(device, (context, cancellationToken)), cancellationToken);
+            }
+        }
+
+        private async Task DiscoveredAsync(UpnpDevice dev, (UpnpDbContext context, CancellationToken cancellationToken) state)
+        {
+            var (ctx, token) = state;
+
+            var description = await dev.GetDescriptionAsync(token).ConfigureAwait(false);
+
+            var entity = new Device
+            {
+                Udn = description.Udn,
+                Location = description.Location.AbsoluteUri,
+                DeviceType = description.DeviceType,
+                FriendlyName = description.FriendlyName,
+                Manufacturer = description.Manufacturer,
+                Description = description.ModelDescription,
+                ModelName = description.ModelName,
+                ModelNumber = description.ModelNumber,
+                IsOnline = true,
+                Icons = description.Icons.Select(i => new Icon
+                {
+                    Width = i.Width,
+                    Height = i.Height,
+                    Mime = i.Mime,
+                    Url = i.Uri.AbsoluteUri
+                }).ToList(),
+                Services = description.Services.Select(s => new Service
+                {
+                    ServiceId = s.ServiceId,
+                    ServiceType = s.ServiceType,
+                    MetadataUrl = s.MetadataUri.AbsoluteUri,
+                    ControlUrl = s.ControlUri.AbsoluteUri
+                }).ToList()
+            };
+
+            await ctx.AddAsync(entity, token).ConfigureAwait(false);
+
+            await ctx.SaveChangesAsync(token).ConfigureAwait(false);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            using(tcs)
+            {
+                tcs.Cancel();
+                await monitorTask.ConfigureAwait(false);
+            }
         }
 
         #endregion
