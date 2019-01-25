a236a1d71d814a132e3c174d7765e4ae29729f3e
Lapiniot <aossss@gmail.com>
2019-01-25 20:04:17 +0200

    Adapted to new async streams based device enumeration model

--- Web.Upnp.Control/Services/UpnpDiscoveryService.cs
+++ Web.Upnp.Control/Services/UpnpDiscoveryService.cs
@@ -24,46 +24,42 @@ namespace Web.Upnp.Control.Services
         {
             var enumerator = new UpnpDeviceEnumerator(TimeSpan.FromSeconds(30), RootDevice);
 
-            using(var scope = Services.CreateScope())
-            using(var context = scope.ServiceProvider.GetRequiredService<UpnpDbContext>())
+            using var scope = Services.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<UpnpDbContext>();
+
+            await foreach(var dev in enumerator.EnumerateAsync(stoppingToken).ConfigureAwait(false))
             {
-                await enumerator.DiscoverAsync(DiscoveredAsync, context, stoppingToken);
+                var d = await dev.GetDescriptionAsync(stoppingToken).ConfigureAwait(false);
+                var entity = new Device
+                {
+                    Udn = d.Udn,
+                    Location = d.Location.AbsoluteUri,
+                    DeviceType = d.DeviceType,
+                    FriendlyName = d.FriendlyName,
+                    Manufacturer = d.Manufacturer,
+                    Description = d.ModelDescription,
+                    ModelName = d.ModelName,
+                    ModelNumber = d.ModelNumber,
+                    IsOnline = true,
+                    Icons = d.Icons.Select(i => new Icon
+                    {
+                        Width = i.Width,
+                        Height = i.Height,
+                        Mime = i.Mime,
+                        Url = i.Uri.AbsoluteUri
+                    }).ToList(),
+                    Services = d.Services.Select(s => new Service
+                    {
+                        ServiceId = s.ServiceId,
+                        ServiceType = s.ServiceType,
+                        MetadataUrl = s.MetadataUri.AbsoluteUri,
+                        ControlUrl = s.ControlUri.AbsoluteUri
+                    }).ToList()
+                };
+
+                await context.AddAsync(entity, stoppingToken).ConfigureAwait(false);
+                await context.SaveChangesAsync(stoppingToken).ConfigureAwait(false);
             }
-        }
-
-        private async Task DiscoveredAsync(UpnpDevice dev, UpnpDbContext ctx, CancellationToken cancellationToken)
-        {
-            var description = await dev.GetDescriptionAsync(cancellationToken).ConfigureAwait(false);
-
-            var entity = new Device
-            {
-                Udn = description.Udn,
-                Location = description.Location.AbsoluteUri,
-                DeviceType = description.DeviceType,
-                FriendlyName = description.FriendlyName,
-                Manufacturer = description.Manufacturer,
-                Description = description.ModelDescription,
-                ModelName = description.ModelName,
-                ModelNumber = description.ModelNumber,
-                IsOnline = true,
-                Icons = description.Icons.Select(i => new Icon
-                {
-                    Width = i.Width,
-                    Height = i.Height,
-                    Mime = i.Mime,
-                    Url = i.Uri.AbsoluteUri
-                }).ToList(),
-                Services = description.Services.Select(s => new Service
-                {
-                    ServiceId = s.ServiceId,
-                    ServiceType = s.ServiceType,
-                    MetadataUrl = s.MetadataUri.AbsoluteUri,
-                    ControlUrl = s.ControlUri.AbsoluteUri
-                }).ToList()
-            };
-
-            await ctx.AddAsync(entity, cancellationToken).ConfigureAwait(false);
-            await ctx.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
         }
     }
 }
