a1470d8f196c654b1d6d4eb914b3c5d767852436
Lapiniot <aossss@gmail.com>
2019-06-23 17:46:59 +0300

    Nuget packages updated + breaking changes solved

---
+++ Web.Upnp.Control/Services/HttpClients/TypedHttpClientsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using static System.Net.DecompressionMethods;
+
+namespace Web.Upnp.Control.Services.HttpClients
+{
+    public static class TypedHttpClientsExtensions
+    {
+        public static IServiceCollection AddImageProxyHttpClient(this IServiceCollection services)
+        {
+            services.AddHttpClient<ImageLoaderProxyClient>(c => c.DefaultRequestHeaders.ConnectionClose = false)
+                .SetHandlerLifetime(TimeSpan.FromMinutes(10))
+                .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
+                {
+                    AutomaticDecompression = None,
+                    MaxConnectionsPerServer = 1
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddSoapHttpClient(this IServiceCollection services)
+        {
+            services.AddHttpClient<HttpSoapClient>(c => c.DefaultRequestHeaders.Add("Accept-Encoding", "gzip,deflate"))
+                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
+                {
+                    AutomaticDecompression = GZip | Deflate,
+                    UseProxy = false,
+                    Proxy = null,
+                    UseCookies = false,
+                    CookieContainer = null,
+                    AllowAutoRedirect = false
+                });
+
+            return services;
+        }
+    }
+}
--- Web.Upnp.Control/Startup.cs
+++ Web.Upnp.Control/Startup.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Net.Http;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +9,6 @@ using Web.Upnp.Control.DataAccess;
 using Web.Upnp.Control.Formatters;
 using Web.Upnp.Control.Services;
 using Web.Upnp.Control.Services.HttpClients;
-using static System.Net.DecompressionMethods;
 
 namespace Web.Upnp.Control
 {
@@ -27,51 +24,22 @@ namespace Web.Upnp.Control
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<UpnpDbContext>(p => p.UseInMemoryDatabase("UpnpDB"));
+            services.AddDbContext<UpnpDbContext>(p => p.UseInMemoryDatabase("UpnpDB"))
+                .AddHostedService<UpnpDiscoveryService>()
+                .AddScoped<IUpnpServiceFactory, UpnpServiceFactory>()
+                .AddImageProxyHttpClient()
+                .AddSoapHttpClient();
 
-            services.AddHostedService<UpnpDiscoveryService>();
-
-            services.AddScoped<IUpnpServiceFactory, UpnpServiceFactory>();
-
-            services.AddSpaStaticFiles(config => { config.RootPath = "ClientApp/build"; });
-
-            services.AddResponseCaching();
-
-            services.AddMvcCore(c =>
+            services.AddControllers(c =>
             {
                 c.OutputFormatters.Insert(c.OutputFormatters.Count - 1, new SystemTextJsonDictionaryOutputFormatter());
                 c.OutputFormatters.Insert(c.OutputFormatters.Count - 1, new HttpResponseMessageFormatter());
             });
 
-            ConfigureHttpClients(services);
+            services.AddResponseCaching();
+            services.AddSpaStaticFiles(config => { config.RootPath = "ClientApp/build"; });
         }
 
-        private static void ConfigureHttpClients(IServiceCollection services)
-        {
-            services.AddHttpClient<HttpClient>(nameof(ImageLoaderProxyClient),
-                    c => c.DefaultRequestHeaders.ConnectionClose = false)
-                .SetHandlerLifetime(TimeSpan.FromMinutes(10))
-                .AddTypedClient<ImageLoaderProxyClient>()
-                .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
-                {
-                    AutomaticDecompression = None,
-                    MaxConnectionsPerServer = 1
-                });
-
-            services.AddHttpClient<HttpClient>(nameof(HttpSoapClient),
-                    c => c.DefaultRequestHeaders.Add("Accept-Encoding", "gzip,deflate"))
-                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
-                .AddTypedClient<HttpSoapClient>()
-                .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
-                {
-                    AutomaticDecompression = GZip | Deflate,
-                    UseProxy = false,
-                    Proxy = null,
-                    UseCookies = false,
-                    CookieContainer = null,
-                    AllowAutoRedirect = false
-                });
-        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -81,7 +49,8 @@ namespace Web.Upnp.Control
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvcWithDefaultRoute()
+            app.UseRouting()
+                .UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute())
                 .UseHttpsRedirection()
                 .UseStaticFiles()
                 .UseResponseCaching();
