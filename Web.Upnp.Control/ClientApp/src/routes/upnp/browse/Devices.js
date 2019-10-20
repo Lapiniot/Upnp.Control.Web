b1849457cdde40735e3d16601c62f4ebb5a46de4
Lapiniot <aossss@gmail.com>
2019-10-20 14:34:35 +0300

    add "Browse" link to the "Xiaomi Network Speaker" devices

--- Web.Upnp.Control/ClientApp/src/routes/upnp/browse/Devices.js
+++ Web.Upnp.Control/ClientApp/src/routes/upnp/browse/Devices.js
@@ -17,25 +17,27 @@ class UpnpDevice extends React.Component {
 
         const { "data-source": d, "data-row-id": id } = this.props;
 
-        const isMediaServer = d.services && d.services.some(s => s.type === "urn:schemas-upnp-org:service:ContentDirectory:1");
+        const isMediaServer = d.services && d.services.some(s =>
+            s.id === "urn:upnp-org:serviceId:ContentDirectory" ||
+            s.id === "urn:xiaomi-com:serviceId:Playlist");
 
         return <div className="card">
-                   <div className="card-header d-flex flex-row">
-                       <DeviceIcon icon={d.icons && d.icons.find(i => i.w <= 48)} alt={d.name} service={d.type} />
-                       <div>
-                           <h5 className="card-title">{d.name}</h5>
-                           <h6 className="card-subtitle">{d.description}</h6>
-                       </div>
-                   </div>
-                   <div className="card-body">
-                       <DeviceInfo data-source={d} />
-                       <ServicesList data-source={d.services} data-row-id={id} />
-                   </div>
-                   <div className="card-footer">
-                       <a className="card-link d-inline-block" href={d.url}><Icon glyph="download" className="x-fa-w-2" />Metadata</a>
-                       {isMediaServer && <RouteLink to={`/upnp/browse/${d.udn}`} glyph="folder" className="card-link d-inline-block">Browse</RouteLink>}
-                   </div>
-               </div>;
+            <div className="card-header d-flex flex-row">
+                <DeviceIcon icon={d.icons && d.icons.find(i => i.w <= 48)} alt={d.name} service={d.type} />
+                <div>
+                    <h5 className="card-title">{d.name}</h5>
+                    <h6 className="card-subtitle">{d.description}</h6>
+                </div>
+            </div>
+            <div className="card-body">
+                <DeviceInfo data-source={d} />
+                <ServicesList data-source={d.services} data-row-id={id} />
+            </div>
+            <div className="card-footer">
+                <a className="card-link d-inline-block" href={d.url}><Icon glyph="download" className="x-fa-w-2" />Metadata</a>
+                {isMediaServer && <RouteLink to={`/upnp/browse/${d.udn}`} glyph="folder" className="card-link d-inline-block">Browse</RouteLink>}
+            </div>
+        </div>;
     }
 }
 
