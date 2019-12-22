9f5969edb50e5b39ae6d19e29804f09ec6379f0d
Lapiniot <aossss@gmail.com>
2019-12-22 18:05:41 +0200

    async Task Main()

--- Web.Upnp.Control/Program.cs
+++ Web.Upnp.Control/Program.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace Web.Upnp.Control
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static Task Main(string[] args)
         {
-            Host.CreateDefaultBuilder(args)
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webHostBuilder => webHostBuilder.UseStartup<Startup>())
                 .UseWindowsService()
                 .UseSystemd()
                 .Build()
-                .Run();
+                .RunAsync();
         }
     }
 }
