cd9c5d19f7d912ed81aa401f51e813a85b509e52
Lapiniot <aossss@gmail.com>
2020-03-13 13:15:01 +0200

    SelectionService now implements EventTarget

--- Web.Upnp.Control/ClientApp/src/components/SelectionService.js
+++ Web.Upnp.Control/ClientApp/src/components/SelectionService.js
@@ -1,21 +1,32 @@
-export default class {
+export default class extends EventTarget {
     constructor() {
+        super();
         this.map = new Map();
+        this.eventInstance = new CustomEvent('changed');
     }
 
     any = () => this.map.size > 0;
 
     one = () => this.map.size === 1;
 
-    select = (key, state = true) => state ? this.map.set(key, true) : this.map.delete(key);
+    select = (key, state = true) => {
+        state ? this.map.set(key, true) : this.map.delete(key);
+        this.dispatchEvent(this.eventInstance);
+    }
 
-    selectMany = (keys, state = true) => keys.forEach(state ? key => this.map.set(key, true) : key => this.map.delete(key));
+    selectMany = (keys, state = true) => {
+        keys.forEach(state ? key => this.map.set(key, true) : key => this.map.delete(key));
+        this.dispatchEvent(this.eventInstance);
+    }
 
     selected = key => this.map.has(key) && this.map.get(key);
 
     all = keys => keys.length > 0 && keys.every(keys => this.selected(keys));
 
-    clear = () => this.map.clear();
+    clear = () => {
+        this.map.clear();
+        this.dispatchEvent(this.eventInstance);
+    }
 
     get keys() {
         return this.map.keys();
