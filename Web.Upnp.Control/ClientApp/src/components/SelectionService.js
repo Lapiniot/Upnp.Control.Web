d8db14c18135b7e73136a13683260b875a9cdbc2
Lapiniot <aossss@gmail.com>
2020-03-16 09:50:39 +0200

    cancellation via event support

--- Web.Upnp.Control/ClientApp/src/components/SelectionService.js
+++ Web.Upnp.Control/ClientApp/src/components/SelectionService.js
@@ -1,34 +1,43 @@
-export default class extends EventTarget {
+export default class SelectionService extends EventTarget {
     constructor() {
         super();
         this.map = new Map();
-        this.eventInstance = new CustomEvent('changed');
+    }
+
+    static createEvent = (detail) => {
+        return new CustomEvent('changed', { detail: detail, cancelable: true });
     }
 
     any = () => this.map.size > 0;
 
     one = () => this.map.size === 1;
 
-    select = (key, state = true) => {
+    none = () => this.map.size === 0;
+
+    select = (key, state = true, detail = null) => {
         state ? this.map.set(key, true) : this.map.delete(key);
-        this.dispatchEvent(this.eventInstance);
+        return this.dispatchEvent(SelectionService.createEvent(detail));
     }
 
-    selectMany = (keys, state = true) => {
+    selectMany = (keys, state = true, detail = null) => {
         keys.forEach(state ? key => this.map.set(key, true) : key => this.map.delete(key));
-        this.dispatchEvent(this.eventInstance);
+        return this.dispatchEvent(SelectionService.createEvent(detail));
     }
 
     selected = key => this.map.has(key) && this.map.get(key);
 
     all = keys => keys.length > 0 && keys.every(keys => this.selected(keys));
 
-    clear = () => {
+    clear = (detail) => {
         this.map.clear();
-        this.dispatchEvent(this.eventInstance);
+        return this.dispatchEvent(SelectionService.createEvent(detail));
     }
 
     get keys() {
         return this.map.keys();
+    }
+
+    get length() {
+        return this.map.size;
     }
 }
