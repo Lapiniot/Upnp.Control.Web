0eb96a32b9e78969b50166ec843c5556e11e4581
Lapiniot <aossss@gmail.com>
2020-03-29 12:33:55 +0300

    NT header moved to client.DefaultRequestHeaders

--- Web.Upnp.Control/Services/HttpClients/EventSubscribeClient.cs
+++ Web.Upnp.Control/Services/HttpClients/EventSubscribeClient.cs
@@ -12,18 +12,14 @@ namespace Web.Upnp.Control.Services.HttpClients
         public EventSubscribeClient(HttpClient client)
         {
             this.client = client ?? throw new ArgumentNullException(nameof(client));
+            this.client.DefaultRequestHeaders.Add("NT", "upnp:event");
         }
 
         public async Task SubscribeAsync(Uri eventsUrl, Uri callbackUrl, TimeSpan timeout, CancellationToken cancellationToken = default)
         {
             using var request = new HttpRequestMessage(new HttpMethod("SUBSCRIBE"), eventsUrl)
             {
-                Headers =
-                {
-                    { "CALLBACK", $"<{callbackUrl.AbsoluteUri}>" },
-                    { "NT", "upnp:event" },
-                    { "TIMEOUT", $"Second-{timeout.TotalSeconds}" }
-                }
+                Headers = { { "CALLBACK", $"<{callbackUrl.AbsoluteUri}>" }, { "TIMEOUT", $"Second-{timeout.TotalSeconds}" } }
             };
             using var response = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
