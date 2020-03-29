bd784633ebd5b40874e172208e43a4fd05bf5d2c
Lapiniot <aossss@gmail.com>
2020-03-29 20:42:07 +0300

    'NOTIFY' verb support

---
+++ Web.Upnp.Control/HttpNotifyAttribute.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace Web.Upnp.Control
+{
+    public class HttpNotifyAttribute : HttpMethodAttribute
+    {
+        public HttpNotifyAttribute(string template) : base(new[] {"NOTIFY"}, template)
+        {
+        }
+
+        public HttpNotifyAttribute() : this("")
+        {
+        }
+    }
+}
