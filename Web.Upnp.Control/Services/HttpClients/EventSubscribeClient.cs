ac186a4f50a7c3f174a8fbf3a3c29adf7d3b05b6
Lapiniot <aossss@gmail.com>
2020-04-05 16:58:21 +0300

    EventSubscribeClient: RenewAsync & UnsubscribeAsync implemented

--- Web.Upnp.Control/Services/HttpClients/EventSubscribeClient.cs
+++ Web.Upnp.Control/Services/HttpClients/EventSubscribeClient.cs
@@ -21,19 +21,44 @@ namespace Web.Upnp.Control.Services.HttpClients
         {
             serverAddresses = (server ?? throw new ArgumentNullException(nameof(server))).Features.Get<IServerAddressesFeature>();
             this.client = client ?? throw new ArgumentNullException(nameof(client));
-            this.client.DefaultRequestHeaders.Add("NT", "upnp:event");
         }
 
-        public async Task SubscribeAsync(Uri eventsUrl, Uri callbackUrl, TimeSpan timeout, CancellationToken cancellationToken = default)
+        public async Task<(string Sid, int Timeout)> SubscribeAsync(Uri subscribeUri, Uri deliveryUri, TimeSpan timeout, CancellationToken cancellationToken = default)
         {
-            if(!callbackUrl.IsAbsoluteUri)
+            if(!deliveryUri.IsAbsoluteUri)
             {
-                callbackUrl = new Uri(bindingAddress ??= ResolveExternalBindingAddress(), callbackUrl);
+                deliveryUri = new Uri(bindingAddress ??= ResolveExternalBindingAddress(), deliveryUri);
             }
 
-            using var request = new HttpRequestMessage(new HttpMethod("SUBSCRIBE"), eventsUrl)
+            using var request = new HttpRequestMessage(new HttpMethod("SUBSCRIBE"), subscribeUri)
             {
-                Headers = {{"CALLBACK", $"<{callbackUrl.AbsoluteUri}>"}, {"TIMEOUT", $"Second-{timeout.TotalSeconds}"}}
+                Headers = { { "NT", "upnp:event" }, { "CALLBACK", $"<{deliveryUri.AbsoluteUri}>" }, { "TIMEOUT", $"Second-{timeout.TotalSeconds}" } }
+            };
+
+            using var response = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+            return (response.Headers.GetValues("SID").First(), int.Parse(response.Headers.GetValues("TIMEOUT").Single()[7..]));
+        }
+
+        public async Task<(string Sid, int Timeout)> RenewAsync(Uri subscribeUri, string sid, TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            using var request = new HttpRequestMessage(new HttpMethod("SUBSCRIBE"), subscribeUri)
+            {
+                Headers = { { "SID", sid }, { "TIMEOUT", $"Second-{timeout.TotalSeconds}" } }
+            };
+
+            using var response = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+            return (response.Headers.GetValues("SID").First(), int.Parse(response.Headers.GetValues("TIMEOUT").Single()[7..]));
+        }
+
+        public async Task UnsubscribeAsync(Uri subscribeUri, string sid, CancellationToken cancellationToken)
+        {
+            using var request = new HttpRequestMessage(new HttpMethod("UNSUBSCRIBE"), subscribeUri)
+            {
+                Headers = { { "SID", sid } }
             };
 
             using var response = await client.SendAsync(request, cancellationToken).ConfigureAwait(false);
