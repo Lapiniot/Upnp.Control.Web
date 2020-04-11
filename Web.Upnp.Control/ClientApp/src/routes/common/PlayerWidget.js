1862e85f9bdaf862ed52f93eb2711a37b017b003
Lapiniot <aossss@gmail.com>
2020-04-11 16:27:39 +0300

    Volume control toolbar UI draft

--- Web.Upnp.Control/ClientApp/src/routes/common/PlayerWidget.js
+++ Web.Upnp.Control/ClientApp/src/routes/common/PlayerWidget.js
@@ -50,16 +50,27 @@ class PlayerCore extends React.Component {
                 <div className="progress my-1" style={{ height: "3px" }}>
                     <div className="progress-bar" role="progressbar" style={{ width: "25%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" />
                 </div>
-                <div className="d-flex align-items-center">
-                    <Toolbar className="bg-normal">
-                        <Toolbar.Group>
-                            <Toolbar.Button title="Prev" onClick={this.prev} glyph="step-backward fa-sm" className="px-1" disabled={!actions.includes("Previous")} />
-                            {actions.includes("Play") && <Toolbar.Button title="Play" onClick={this.play} glyph="play-circle" className="fa-2x" />}
-                            {(actions.includes("Pause") || transitioning) && <Toolbar.Button title="Pause" onClick={this.pause} glyph="pause-circle" className="fa-2x" disabled={transitioning} />}
-                            <Toolbar.Button title={next ? `Next: ${next.title}` : "Next"} onClick={this.next} glyph="step-forward fa-sm" className="px-1" disabled={!actions.includes("Next")} />
+                <div className="d-flex align-items-center justify-content-between">
+                    <Toolbar>
+                        <Toolbar.Group className="align-items-center">
+                            <Toolbar.Button title="Prev" onClick={this.prev} glyph="step-backward fa-sm" className="p-0 px-1" disabled={!actions.includes("Previous")} />
+                            {actions.includes("Play") && <Toolbar.Button title="Play" onClick={this.play} glyph="play-circle" className="fa-2x p-0 px-1" />}
+                            {(actions.includes("Pause") || transitioning) && <Toolbar.Button title="Pause" onClick={this.pause} glyph="pause-circle" className="fa-2x p-0 px-1" disabled={transitioning} />}
+                            <Toolbar.Button title={next ? `Next: ${next.title}` : "Next"} onClick={this.next} glyph="step-forward fa-sm" className="p-0 px-1" disabled={!actions.includes("Next")} />
                         </Toolbar.Group>
                     </Toolbar>
-                    {current && <div className="flex-fill text-center">{current.title}</div>}
+                    {current && <div className="text-center align-middle mx-2 overflow-hidden" title={JSON.stringify(current)}>
+                        <h6 className="text-truncate" title={current.title}>{current.title}</h6>
+                    </div>}
+                    <Toolbar className="flex-nowrap">
+                        <Toolbar.Group>
+                            <Toolbar.Button glyph="random" className="p-1" />
+                            <Toolbar.Button glyph="retweet" className="p-1" />
+                        </Toolbar.Group>
+                        <Toolbar.Group>
+                            <Toolbar.Button glyph="volume-off" className="p-2" />
+                        </Toolbar.Group>
+                    </Toolbar>
                 </div>
             </div>
         </>;
