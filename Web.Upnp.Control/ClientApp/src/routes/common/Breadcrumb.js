db76a61cf716d2ef9fead00811538a7dc9dde189
Lapiniot <aossss@gmail.com>
2020-04-11 13:23:38 +0300

    minor UI fixes in Breadcrumb component

--- Web.Upnp.Control/ClientApp/src/routes/common/Breadcrumb.js
+++ Web.Upnp.Control/ClientApp/src/routes/common/Breadcrumb.js
@@ -7,10 +7,11 @@ const BreadcrumbItem = ({ active, title, url }) => active
     : <li className="breadcrumb-item"><NavLink to={url}>{title}</NavLink></li>;
 
 export default ({ dataContext = [], baseUrl }) =>
-    <nav className="position-sticky sticky-top" aria-label="breadcrumb">
+    <nav aria-label="breadcrumb">
         <ol className="breadcrumb rounded-0 my-0 p-2">
             {[
                 reversemap(dataContext, ({ title, id }, i) => <BreadcrumbItem key={i} title={title} url={`${baseUrl}/${id}`} active={i === 0} />)
             ]}
+            &nbsp;
         </ol>
     </nav>;
