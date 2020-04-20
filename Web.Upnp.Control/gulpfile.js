cd92dcb5b5728f4d3bb4174b8f0f34ab505467be
Lapiniot <aossss@gmail.com>
2020-04-20 16:33:16 +0300

    VS Code automation tasks improved

--- Web.Upnp.Control/gulpfile.js
+++ Web.Upnp.Control/gulpfile.js
@@ -1,10 +1,17 @@
-/// <binding BeforeBuild='SASS' />
 var gulp = require("gulp"), sass = require("gulp-sass");
 
-const sources = [
-    "./ClientApp/src/styles/index.scss"
-];
+const sources = ["./ClientApp/src/styles/*.scss"];
 
-const destination = "./ClientApp/src/css";
+gulp.task('sass', function (cb) {
+    gulp.src(sources)
+        .pipe(sass())
+        .pipe(gulp.dest(f => "./ClientApp/src/css/"));
+    cb();
+});
 
-gulp.task("SASS", () => gulp.src(sources).pipe(sass()).pipe(gulp.dest(destination)));
+gulp.task('watch',
+    gulp.series('sass', function (cb) {
+        gulp.watch(sources, gulp.series('sass'));
+        cb();
+    })
+);
