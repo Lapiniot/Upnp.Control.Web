1a9ce337015469c467b6f27aab019c42ec34ce3f
Lapiniot <aossss@gmail.com>
2020-09-26 19:12:25 +0300

    progress control animation management has been moved close to the component's code

--- Web.Upnp.Control/ClientApp/src/routes/common/Progress.js
+++ Web.Upnp.Control/ClientApp/src/routes/common/Progress.js
@@ -18,15 +18,19 @@ export class Progress extends React.Component {
         const total = parseMilliseconds(duration);
         const current = parseMilliseconds(time);
 
-        if(!total) return null;
+        if (!total) return null;
 
         const progress = total > 0 ? Math.round(current * 100 / total) : 0;
+
+        const style = running
+            ? { width: `${progress}%`, animation: `inflate-width ${total - current}ms linear forwards` }
+            : { width: `${progress}%` };
 
         return <div className="d-flex flex-wrap justify-content-between user-select-none" onClick={this.clickHandler}>
             <Timer className="text-tiny" current={current / 1000} total={total / 1000} running={running} />
             <small className="text-tiny">{formatTime(total / 1000)}</small>
-            <div className="slider my-2 flex-basis-100" data-running={running}>
-                <div className="slider-line" style={{ width: `${progress}%`, animationDuration: `${running ? (total - current) : 0}ms` }} />
+            <div className="slider my-2 flex-basis-100">
+                <div className="slider-line" style={style} />
                 <div className="slider-ticker" />
             </div>
         </div>;
