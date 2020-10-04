bd41930ff26071563bf9d914e66b9cf7e41a24cf
Lapiniot <aossss@gmail.com>
2020-10-04 22:27:48 +0300

    fixed bug with messy progress running animations on change + minor progress styling improvements

--- Web.Upnp.Control/ClientApp/src/routes/common/Progress.js
+++ Web.Upnp.Control/ClientApp/src/routes/common/Progress.js
@@ -4,6 +4,11 @@ import Timer from "../../components/Timer";
 
 export class Progress extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.spin = 0;
+    }
+
     clickHandler = e => {
         const element = e.currentTarget;
         const rect = element.getBoundingClientRect();
@@ -22,11 +27,15 @@ export class Progress extends React.Component {
 
         const progress = total > 0 ? Math.round(current * 100 / total) : 0;
 
+        // we constantly switch between animations with same settings, but different names 'inflate-width-0' and
+        // 'inflate-width-1' e.g. - this is a trick in order to apply new animation resetting running one
         const style = running
-            ? { width: `${progress}%`, animation: `inflate-width ${total - current}ms linear forwards` }
+            ? { width: `${progress}%`, animation: `inflate-width-${this.spin} ${total - current}ms linear forwards` }
             : { width: `${progress}%`, animation: "none" };
 
-        return <div className="d-flex flex-wrap justify-content-between user-select-none" onClick={this.clickHandler}>
+        this.spin ^= 0x01;
+
+        return <div className="d-flex flex-wrap justify-content-between user-select-none" role="button" onClick={this.clickHandler}>
             <Timer className="text-tiny" current={current / 1000} total={total / 1000} running={running} />
             <small className="text-tiny">{formatTime(total / 1000)}</small>
             <div className="slider my-2 flex-basis-100">
