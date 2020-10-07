575c32490cd1502c1b7a7ed6e39456f17066c08c
Lapiniot <aossss@gmail.com>
2020-10-07 15:45:12 +0300

    immutability fix for Device entity

--- Web.Upnp.Control/Models/Device.cs
+++ Web.Upnp.Control/Models/Device.cs
@@ -6,7 +6,7 @@ namespace Web.Upnp.Control.Models
     public record Device(string Udn, Uri Location, string DeviceType, string FriendlyName, string Manufacturer,
         string Description, string ModelName, string ModelNumber, DateTimeOffset? ExpiresAt)
     {
-        public ICollection<Icon> Icons { get; set; }
-        public ICollection<Service> Services { get; set; }
+        public ICollection<Icon> Icons { get; init; }
+        public ICollection<Service> Services { get; init; }
     };
 }
