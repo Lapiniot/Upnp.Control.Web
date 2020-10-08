ec6a9a6d5c987e356d4a46cff5be6bf40a648022
Lapiniot <aossss@gmail.com>
2020-10-08 12:31:57 +0300

    fix for broken services list after id property rename

--- Web.Upnp.Control/ClientApp/src/routes/upnp/browse/Devices.js
+++ Web.Upnp.Control/ClientApp/src/routes/upnp/browse/Devices.js
@@ -9,26 +9,27 @@ import ServicesList from "../../common/DeviceServiceList";
 import DeviceList from "../../common/DeviceList";
 
 function UpnpDevice({ "data-source": d, "data-row-id": id }) {
-    
-    const isMediaServer = d.services && d.services.some(s => s.id === "urn:upnp-org:serviceId:ContentDirectory" || s.id === "urn:xiaomi-com:serviceId:Playlist");
+
+    const isMediaServer = d.services && d.services.some(
+        s => s.usn === "urn:upnp-org:serviceId:ContentDirectory" || s.usn === "urn:xiaomi-com:serviceId:Playlist");
 
     return <div className="card">
-               <div className="card-header d-flex">
-                   <DeviceIcon icon={d.icons && d.icons.find(i => i.w <= 48)} alt={d.name} service={d.type} />
-                   <div>
-                       <h5 className="card-title">{d.name}</h5>
-                       <h6 className="card-subtitle">{d.description}</h6>
-                   </div>
-               </div>
-               <div className="card-body">
-                   <DeviceInfo data-source={d} />
-                   <ServicesList data-source={d.services} data-row-id={id} />
-               </div>
-               <div className="card-footer no-decoration d-flex justify-content-end">
-                   <a href={d.url}><i className="fas x-fa-w-2 fa-download"/>Metadata</a>
-                   {isMediaServer && <RouteLink to={`/upnp/browse/${d.udn}`} glyph="folder">Browse</RouteLink>}
-               </div>
-           </div>;
+        <div className="card-header d-flex">
+            <DeviceIcon icon={d.icons && d.icons.find(i => i.w <= 48)} alt={d.name} service={d.type} />
+            <div>
+                <h5 className="card-title">{d.name}</h5>
+                <h6 className="card-subtitle">{d.description}</h6>
+            </div>
+        </div>
+        <div className="card-body">
+            <DeviceInfo data-source={d} />
+            <ServicesList data-source={d.services} data-row-id={id} />
+        </div>
+        <div className="card-footer no-decoration d-flex justify-content-end">
+            <a href={d.url}><i className="fas x-fa-w-2 fa-download" />Metadata</a>
+            {isMediaServer && <RouteLink to={`/upnp/browse/${d.udn}`} glyph="folder">Browse</RouteLink>}
+        </div>
+    </div>;
 }
 
 const dataUrl = $api.discover("upnp").url();
