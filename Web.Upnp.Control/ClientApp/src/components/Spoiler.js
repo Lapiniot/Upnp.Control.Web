7bfa09b7b86b3320c7c72b9e4ff7aba52239768d
Lapiniot <aossss@gmail.com>
2020-10-10 17:59:28 +0300

    fix: missing togle button image in Spoiler

--- Web.Upnp.Control/ClientApp/src/components/Spoiler.js
+++ Web.Upnp.Control/ClientApp/src/components/Spoiler.js
@@ -3,9 +3,10 @@ import "bootstrap/js/src/collapse";
 
 export default ({ uniqueId, title, children }) =>
     <div className="spoiler">
-        <button className="btn-light collapsed" aria-expanded="false" aria-controls={uniqueId}
-                data-toggle="collapse" data-target={`#${uniqueId}`}>
-            <i />{title}
+        <button type="button" className="btn btn-block btn-light collapsed"
+            data-toggle="collapse" data-target={`#${uniqueId}`}
+            aria-controls={uniqueId} aria-expanded="false">
+            <i className="fa-fw" />{title}
         </button>
         <div className="collapse" id={uniqueId}>{children}</div>
     </div>;
