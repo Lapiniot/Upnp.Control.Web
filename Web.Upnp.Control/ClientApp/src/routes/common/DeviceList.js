723d843b33066937b7beea662e40df08d2854a2e
Lapiniot <aossss@gmail.com>
2020-10-11 17:29:05 +0300

    device list grid visual layout improved (uses auto-columns grid layout)

--- Web.Upnp.Control/ClientApp/src/routes/common/DeviceList.js
+++ Web.Upnp.Control/ClientApp/src/routes/common/DeviceList.js
@@ -1,6 +1,6 @@
 ﻿import React from "react";
 
 export default ({ dataContext: { source: data }, itemTemplate: Item }) =>
-    <div className="d-grid grid-c1 grid-lg-c2 grid-xxl-c3 grid-xxxxl-c4 m-3">
+    <div className="d-grid grid-auto-c3 align-items-start justify-content-evenly m-3">
         {[data.map((item, index) => <Item key={index} data-source={item} data-row-id={index} />)]}
     </div>;
