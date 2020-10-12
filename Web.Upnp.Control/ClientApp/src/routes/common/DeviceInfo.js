017ca681934136733d471fc4fbe0ac8b1da97dd9
Lapiniot <aossss@gmail.com>
2020-10-12 20:26:53 +0300

    1. form-table style eliminated in favour to grid layout 2. Media item icon on-hover animation

--- Web.Upnp.Control/ClientApp/src/routes/common/DeviceInfo.js
+++ Web.Upnp.Control/ClientApp/src/routes/common/DeviceInfo.js
@@ -1,26 +1,16 @@
 import React from "react";
 
-export default function({ "data-source": { udn, type, maker, model, modelNumber } }) {
-    return <div className="form-table">
-               <div>
-                   <div>UDN</div>
-                   <div>{udn}</div>
-               </div>
-               <div>
-                   <div>Type</div>
-                   <div>{type}</div>
-               </div>
-               <div>
-                   <div>Maker</div>
-                   <div>{maker}</div>
-               </div>
-               <div>
-                   <div>Model</div>
-                   <div>{model}</div>
-               </div>
-               <div>
-                   <div>Model #</div>
-                   <div>{modelNumber}</div>
-               </div>
-           </div>;
+export default function ({ "data-source": { udn, type, maker, model, modelNumber } }) {
+    return <div className="grid-form mb-2">
+        <div>UDN</div>
+        <div>{udn}</div>
+        <div>Type</div>
+        <div>{type}</div>
+        <div>Maker</div>
+        <div>{maker}</div>
+        <div>Model</div>
+        <div>{model}</div>
+        <div>Model #</div>
+        <div>{modelNumber}</div>
+    </div>;
 }
