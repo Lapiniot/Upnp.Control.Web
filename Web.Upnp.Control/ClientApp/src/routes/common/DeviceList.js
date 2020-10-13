50ae666f5565e45fca2d99cea365a6161a620d00
Lapiniot <aossss@gmail.com>
2020-10-13 17:13:16 +0300

    React component unmount issue fixed

--- Web.Upnp.Control/ClientApp/src/routes/common/DeviceList.js
+++ Web.Upnp.Control/ClientApp/src/routes/common/DeviceList.js
@@ -2,8 +2,15 @@
 
 export default ({ dataContext: { source: data }, itemTemplate: Item }) =>
     <>
-        <i data-fa-symbol="upnp-renderer" className="fas fa-tv" />
-        <i data-fa-symbol="upnp-server" className="fas fa-server" />
+        <svg className="d-none" aria-hidden="true" focusable="false" role="img">
+            <symbol id="upnp-renderer" viewBox="0 0 640 512">
+                <path fill="currentColor" d="M592 0H48A48 48 0 0 0 0 48v320a48 48 0 0 0 48 48h240v32H112a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H352v-32h240a48 48 0 0 0 48-48V48a48 48 0 0 0-48-48zm-16 352H64V64h512z"></path>
+            </symbol>
+            <symbol id="upnp-server" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M480 160H32c-17.673 0-32-14.327-32-32V64c0-17.673 14.327-32 32-32h448c17.673 0 32 14.327 32 32v64c0 17.673-14.327 32-32 32zm-48-88c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24zm-64 0c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24zm112 248H32c-17.673 0-32-14.327-32-32v-64c0-17.673 14.327-32 32-32h448c17.673 0 32 14.327 32 32v64c0 17.673-14.327 32-32 32zm-48-88c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24zm-64 0c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24zm112 248H32c-17.673 0-32-14.327-32-32v-64c0-17.673 14.327-32 32-32h448c17.673 0 32 14.327 32 32v64c0 17.673-14.327 32-32 32zm-48-88c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24zm-64 0c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24z">
+                </path>
+            </symbol>
+        </svg>
         <div className="d-grid grid-auto-x3 align-items-start justify-content-evenly m-3">
             {[data.map((item, index) => <Item key={index} data-source={item} data-row-id={index} />)]}
         </div>
