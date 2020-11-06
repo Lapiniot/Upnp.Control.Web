8ac2539e6176f794064aa2dd2eac314e2efc5ab9
Lapiniot <aossss@gmail.com>
2020-11-06 15:44:35 +0200

    TextValueEditDialog: value edited is passed back as onConfirm calllback parameter

--- Web.Upnp.Control/ClientApp/src/components/Dialogs.js
+++ Web.Upnp.Control/ClientApp/src/components/Dialogs.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Modal from "./Modal";
 
-export function TextValueEditDialog({ id, title, label, defaultValue, confirmText = "OK", onValueChanged, onConfirm, inputRef, ...other }) {
+export function TextValueEditDialog({ id, title, label, defaultValue, confirmText = "OK", onValueChanged, onConfirm, inputRef = React.createRef(), ...other }) {
     return <Modal id={id} title={title} {...other}>
-               <div className="input-group mb-3">
-                   <div className="input-group-prepend">
-                       <span className="input-group-text" id="basic-addon1">{label}</span>
-                   </div>
-                   <input ref={inputRef} type="text" onChange={onValueChanged} className="form-control" defaultValue={defaultValue}
-                          placeholder="[provide value]" aria-label={label} aria-describedby="basic-addon1" />
-               </div>
-               <Modal.Footer>
-                   <Modal.Button key="cancel" text="Cancel" className="btn-secondary" dismiss />
-                   <Modal.Button key="confirm" text={confirmText} className="btn-primary" onClick={onConfirm} dismiss />
-               </Modal.Footer>
-           </Modal>;
+        <div className="input-group mb-3">
+            <div className="input-group-prepend">
+                <span className="input-group-text" id="basic-addon1">{label}</span>
+            </div>
+            <input ref={inputRef} type="text" onChange={onValueChanged} className="form-control" defaultValue={defaultValue}
+                placeholder="[provide value]" aria-label={label} aria-describedby="basic-addon1" />
+        </div>
+        <Modal.Footer>
+            <Modal.Button key="cancel" text="Cancel" className="btn-secondary" dismiss />
+            <Modal.Button key="confirm" text={confirmText} className="btn-primary" onClick={() => onConfirm(inputRef.current.value)} dismiss />
+        </Modal.Footer>
+    </Modal>;
 }
 
 export function ConfirmationDialog({ id, title, confirmText = "OK", onConfirm, children, ...other }) {
     return <Modal id={id} title={title} {...other}>
-               {children}
-               <Modal.Footer>
-                   <Modal.Button key="cancel" text="Cancel" className="btn-secondary" dismiss />
-                   <Modal.Button key="confirm" text={confirmText} className="btn-primary" onClick={onConfirm} dismiss />
-               </Modal.Footer>
-           </Modal>;
+        {children}
+        <Modal.Footer>
+            <Modal.Button key="cancel" text="Cancel" className="btn-secondary" dismiss />
+            <Modal.Button key="confirm" text={confirmText} className="btn-primary" onClick={onConfirm} dismiss />
+        </Modal.Footer>
+    </Modal>;
 }
