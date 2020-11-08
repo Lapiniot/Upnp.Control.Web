f13b0a81d3d9cf6148c9119a4fd1997289fedd83
Lapiniot <aossss@gmail.com>
2020-11-08 12:09:33 +0200

    unnecessary resources excluded

--- Web.Upnp.Control/ClientApp/src/index.js
+++ Web.Upnp.Control/ClientApp/src/index.js
@@ -1,7 +1,6 @@
 ﻿import "./css/index.css";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
 
 import React from "react";
 import ReactDOM from "react-dom";
