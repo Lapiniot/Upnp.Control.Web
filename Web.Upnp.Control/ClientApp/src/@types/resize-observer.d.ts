a1848edf13d739f5c68344cda4bb181c522292e2
Lapiniot <aossss@gmail.com>
2020-11-18 21:25:13 +0200

    missing global window.ResizeObserver type definition

--- Web.Upnp.Control/ClientApp/src/@types/resize-observer.d.ts
+++ Web.Upnp.Control/ClientApp/src/@types/resize-observer.d.ts
@@ -1,11 +1,33 @@
-export { }
-
-interface ResizeObserver {
-}
-
 declare global {
+    type ResizeObserverBoxOptions = "border-box" | "content-box" | "device-pixel-content-box"
+
+    interface ResizeObserverOptions { box?: ResizeObserverBoxOptions }
+
+    interface ResizeObserverSize {
+        readonly inlineSize: number;
+        readonly blockSize: number;
+    }
+
+    interface ResizeObserver {
+        disconnect: () => void;
+        observe: (target: HTMLOrSVGElement, options?: ResizeObserverOptions) => void;
+        unobserve: (target: HTMLOrSVGElement) => void;
+    }
+
+    interface ResizeObserverEntry {
+        target: Element;
+        contentRect: DOMRectReadOnly;
+        borderBoxSize: ReadonlyArray<ResizeObserverSize>;
+        contentBoxSize: ReadonlyArray<ResizeObserverSize>;
+        devicePixelContentBoxSize: ReadonlyArray<ResizeObserverSize>;
+    }
+
+    interface ResizeObserverCallback { (entries: ResizeObserverEntry[], observer: ResizeObserver): void }
+
     var ResizeObserver: {
         prototype: ResizeObserver;
-        new(): ResizeObserver;
+        new(handler: ResizeObserverCallback): ResizeObserver;
     }
 }
+
+export { }
