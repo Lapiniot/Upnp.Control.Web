4e7a80d08edc6aab3a47391d7556003b90bc68db
Lapiniot <aossss@gmail.com>
2020-11-19 22:15:57 +0200

    some UI unrelated props removed

--- Web.Upnp.Control/ClientApp/src/routes/common/DeviceRouter.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/DeviceRouter.tsx
@@ -3,20 +3,28 @@ import { Switch, Route } from "react-router-dom"
 import { RouteComponentProps } from "react-router";
 import Browser from "./BrowseRouter";
 import $api from "../../components/WebApi";
-import { withDataFetch, withMemoKey } from "../../components/DataFetch";
+import { DataFetchProps, withDataFetch, withMemoKey } from "../../components/DataFetch";
 import DeviceCard from "./Device.Upnp";
 import { DataSourceProps, UpnpDevice } from "./Types";
-import { DeviceContainer, DeviceListContainer } from "./DeviceList";
+import { DeviceContainer, DeviceListContainer, TemplatedDataComponentProps } from "./DeviceList";
+
+type CategoryParams = { category: string }
+type DeviceParams = { device: string }
 
 type DeviceRouterProps = PropsWithChildren<{ deviceTemplate?: ComponentType<DataSourceProps<UpnpDevice>> }>
-    & RouteComponentProps<{ category: string; device?: string }>
+    & RouteComponentProps<CategoryParams>
 
-const Device = withDataFetch(DeviceContainer, ({ device, category }) => withMemoKey($api.devices(category as string, device).jsonFetch, `${category}|${device}`), { usePreloader: true });
-const Devices = withDataFetch(DeviceListContainer, ({ category }) => withMemoKey($api.devices(category as string).jsonFetch, category as string), { usePreloader: false });
+const Device = withDataFetch<DataFetchProps<UpnpDevice> & TemplatedDataComponentProps<UpnpDevice>, CategoryParams & DeviceParams>(
+    DeviceContainer, ({ device, category }) => withMemoKey($api.devices(category, device).jsonFetch, `${category}|${device}`));
+
+const Devices = withDataFetch<DataFetchProps<UpnpDevice[]> & TemplatedDataComponentProps<UpnpDevice>, CategoryParams>(
+    DeviceListContainer, ({ category }) => withMemoKey($api.devices(category).jsonFetch, category as string),
+    { usePreloader: false });
 
 export default ({ match: { path, params: { category } }, deviceTemplate = DeviceCard, children }: DeviceRouterProps) => <Switch>
     {children}
     <Route path={`${path}/:device/browse`} render={props => <Browser {...props} />} />
-    <Route path={`${path}/:device`} exact render={({ match: { params: { device } } }: RouteComponentProps<{ category: string; device: string }>) => <Device category={category} itemTemplate={deviceTemplate} device={device} />} />
+    <Route path={`${path}/:device`} exact render={({ match: { params: { device } } }: RouteComponentProps<CategoryParams & DeviceParams>) =>
+        <Device category={category} itemTemplate={deviceTemplate} device={device} />} />
     <Route path={path} exact render={() => <Devices category={category} itemTemplate={deviceTemplate} />} />
 </Switch>
