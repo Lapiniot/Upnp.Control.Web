f99ccb738281c8a9585292f1b504fb5de2d8be8f
Lapiniot <aossss@gmail.com>
2020-11-28 00:02:04 +0200

    Quick playlist access menu styling improved

--- Web.Upnp.Control/ClientApp/src/routes/common/Device.Umi.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/Device.Umi.tsx
@@ -6,6 +6,7 @@ import { BrowseFetchResult, DataSourceProps, UpnpDevice } from "./Types";
 import { fromBaseQuery } from "./BrowserUtils";
 import $api from "../../components/WebApi";
 import { DataFetchProps, withDataFetch } from "../../components/DataFetch";
+import AlbumArt from "./AlbumArt";
 
 function MicroLoader() {
     return <span><i className="fas fa-spinner fa-spin" /></span>
@@ -20,16 +21,17 @@ function playUrlHandler(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
 }
 
 function Menu({ dataContext: d, device }: HTMLAttributes<HTMLDivElement> & DataFetchProps<BrowseFetchResult> & { device?: string }) {
-    return <>
-        <a href="#" className="dropdown-toggle dropdown-toggle-split px-1" data-toggle="dropdown" aria-expanded="false">
-            <span className="visually-hidden">Toggle Dropdown</span>
+    return <div className="btn-group dropright">
+        <a href="#" className="nav-link p-0 px-1" data-toggle="dropdown" aria-expanded="false" title="Quick switch playlists">
+            <i className="fas fa-caret-right fa-lg" /><span className="visually-hidden">Toggle Dropdown</span>
         </a>
         <ul className="dropdown-menu" data-device={device}>
             {d?.source.items.map(i => <li>
-                <a className="dropdown-item" href="#" data-play-url={i.res?.url + "#play"} onClick={playUrlHandler}>{i.title}</a>
+                <a className="dropdown-item" href="#" data-play-url={i.res?.url + "#play"} onClick={playUrlHandler}>
+                    <AlbumArt itemClass={i.class} albumArts={i.albumArts} className="album-art-sm mr-1 align-middle" />{i.title}</a>
             </li>)}
         </ul>
-    </>
+    </div>
 }
 
 const builder = fromBaseQuery((device) => $api.browse(device).get("PL:").withResource().withVendor());
@@ -51,11 +53,9 @@ export default function UmiDevice({ "data-source": { name, description, udn } }:
             <PlayerWidget udn={udn} />
         </div>
         <div className="card-footer d-flex align-items-center gap-2 no-decoration">
-            <RouteLink to={`/umi/${udn}/browse`} glyph="folder" className="p-0">Browse</RouteLink>
-            <div className="btn-group dropright">
-                <RouteLink to={`/umi/${udn}/playlists/PL:`} glyph="list-alt" className="p-0">Playlists</RouteLink>
-                <PlaylistMenu device={udn} />
-            </div>
+            <RouteLink to={`/umi/${udn}/browse`} glyph="folder" className="p-0 btn-link" title="Browse content">Browse</RouteLink>
+            <RouteLink to={`/umi/${udn}/playlists/PL:`} glyph="list-alt" className="p-0" title="Manage playlists">Playlists</RouteLink>
+            <PlaylistMenu device={udn} />
         </div>
     </div>;
 }
