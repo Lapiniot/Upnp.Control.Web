65262102b8f6c92fabd19dc5cac51a35543de98c
Lapiniot <aossss@gmail.com>
2020-11-27 16:49:34 +0200

    Playlists quick access dropdown menu draft

--- Web.Upnp.Control/ClientApp/src/routes/common/Device.Umi.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/Device.Umi.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { HTMLAttributes } from "react";
+import "bootstrap/js/dist/dropdown";
 import { RouteLink } from "../../components/NavLink";
 import PlayerWidget from "./PlayerWidget";
-import { DataSourceProps, UpnpDevice } from "./Types";
+import { BrowseFetchResult, DataSourceProps, UpnpDevice } from "./Types";
+import { fromBaseQuery } from "./BrowserUtils";
+import $api from "../../components/WebApi";
+import { DataFetchProps, withDataFetch } from "../../components/DataFetch";
+
+function MicroLoader() {
+    return <span><i className="fas fa-spinner fa-spin" /></span>
+}
+
+function playUrlHandler(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    const url = e.currentTarget.dataset["playUrl"];
+    const device = e.currentTarget.parentElement?.parentElement?.dataset["device"];
+    if (device && url) $api.control(device).playUri(url).fetch();
+    e.preventDefault();
+    return false;
+}
+
+function Menu({ dataContext: d, device }: HTMLAttributes<HTMLDivElement> & DataFetchProps<BrowseFetchResult> & { device?: string }) {
+    return <>
+        <a href="#" className="dropdown-toggle dropdown-toggle-split px-1" data-toggle="dropdown" aria-expanded="false">
+            <span className="visually-hidden">Toggle Dropdown</span>
+        </a>
+        <ul className="dropdown-menu" data-device={device}>
+            {d?.source.items.map(i => <li>
+                <a className="dropdown-item" href="#" data-play-url={i.res?.url + "#play"} onClick={playUrlHandler}>{i.title}</a>
+            </li>)}
+        </ul>
+    </>
+}
+
+const builder = fromBaseQuery((device) => $api.browse(device).get("PL:").withResource().withVendor());
+
+const PlaylistMenu = withDataFetch(Menu, builder, { template: MicroLoader });
 
 export default function UmiDevice({ "data-source": { name, description, udn } }: DataSourceProps<UpnpDevice>) {
     return <div className="card">
@@ -19,7 +52,10 @@ export default function UmiDevice({ "data-source": { name, description, udn } }:
         </div>
         <div className="card-footer d-flex align-items-center gap-2 no-decoration">
             <RouteLink to={`/umi/${udn}/browse`} glyph="folder" className="p-0">Browse</RouteLink>
-            <RouteLink to={`/umi/${udn}/playlists/PL:`} glyph="list-alt" className="p-0">Playlists</RouteLink>
+            <div className="btn-group dropright">
+                <RouteLink to={`/umi/${udn}/playlists/PL:`} glyph="list-alt" className="p-0">Playlists</RouteLink>
+                <PlaylistMenu device={udn} />
+            </div>
         </div>
     </div>;
 }
