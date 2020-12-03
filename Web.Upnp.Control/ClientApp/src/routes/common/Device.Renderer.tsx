2adbc43ce86a5595017422e90eb561237008e4a1
Lapiniot <aossss@gmail.com>
2020-12-03 19:42:33 +0200

    Open media draft in Device.Renderer component

--- Web.Upnp.Control/ClientApp/src/routes/common/Device.Renderer.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/Device.Renderer.tsx
@@ -1,24 +1,61 @@
-import React from "react";
-import { RouteLink } from "../../components/NavLink";
+import React, { ReactNode } from "react";
+import { BrowserCoreProps } from "./BrowserCore";
+import BrowserDialog, { BrowseResult } from "./BrowserDialog";
+import { DIDLUtils } from "./BrowserUtils";
 import DeviceIcon from "./DeviceIcon";
 import PlayerWidget from "./PlayerWidget";
 import { DataSourceProps, UpnpDevice } from "./Types";
 
-export default function RendererDevice({ "data-source": { name, description, udn, type, icons }, category = "renderers" }:
-    DataSourceProps<UpnpDevice> & { category?: string }) {
-    return <div className="card">
-        <div className="card-header d-flex flex-row">
-            <DeviceIcon service={type} icons={icons} />
-            <div>
-                <h5 className="card-title">{name}</h5>
-                <h6 className="card-subtitle">{description}</h6>
+type RenderDeviceProps = DataSourceProps<UpnpDevice> & {
+    category?: string;
+}
+
+type RenderDeviceState = { modal?: ReactNode | null }
+
+const browserProps: BrowserCoreProps = {
+    filter: DIDLUtils.isMediaItem,
+    selectOnClick: true
+}
+
+export default class RenderDevice extends React.Component<RenderDeviceProps, RenderDeviceState> {
+
+    state = { modal: null }
+
+    resetModal = () => {
+        this.setState({ modal: null });
+    }
+
+    playMedia = (data: BrowseResult) => {
+        this.resetModal();
+    }
+
+    browse = () => {
+        this.setState({
+            modal: <BrowserDialog id="open-media-dialog" title="Select media to play" confirmText="Open" className="modal-lg modal-vh-80"
+                onDismiss={this.resetModal} onConfirm={this.playMedia} browserProps={browserProps} immediate>
+            </BrowserDialog>
+        })
+    }
+
+    render() {
+
+        const { "data-source": { name, description, udn, type, icons }, category = "renderers" } = this.props;
+
+        return <div className="card">
+            <div className="card-header d-flex flex-row">
+                <DeviceIcon service={type} icons={icons} />
+                <div>
+                    <h5 className="card-title">{name}</h5>
+                    <h6 className="card-subtitle">{description}</h6>
+                </div>
             </div>
+            <div className="card-body">
+                <PlayerWidget udn={udn} />
+            </div>
+            <div className="card-footer no-decoration d-flex gap-2">
+                <button type="button" className="btn nav-link btn-link p-0" onClick={this.browse}>Open media</button>
+            </div>
+            {this.state.modal}
         </div>
-        <div className="card-body">
-            <PlayerWidget udn={udn} />
-        </div>
-        <div className="card-footer no-decoration d-flex gap-2">
-            <RouteLink to={`/${category}/${udn}/browse`} glyph="stream" className="p-0" disabled>Open media</RouteLink>
-        </div>
-    </div>;
+    }
 }
