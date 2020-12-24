d69db2f22f1916a723c38adccc99c7a4a367f81f
Lapiniot <aossss@gmail.com>
2020-12-25 00:21:19 +0200

    more elegant toasts are used instead of dumb alerts since now

--- Web.Upnp.Control/ClientApp/src/routes/common/DeviceList.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/DeviceList.tsx
@@ -1,24 +1,9 @@
 ﻿import React, { ComponentType } from "react";
-import "bootstrap/js/src/alert";
 import { SignalRListener, SignalRMessageHandler } from "../../components/SignalR";
 import { LoadIndicatorOverlay } from "../../components/LoadIndicator";
 import { DataSourceProps, UpnpDevice } from "./Types";
 import { DataFetchProps } from "../../components/DataFetch";
-
-type AlertProps = {
-    type: string;
-    name: string;
-    description: string;
-    onDismiss?: () => void | undefined;
-};
-
-function DiscoveryAlert({ type, name, description, onDismiss }: AlertProps) {
-    const appeared = type === "appeared";
-    return <div className={`alert ${appeared ? "alert-success" : "alert-warning"} alert-dismissible fade show m-3 d-flex justify-content-center`} role="alert">
-        <h6>{name}&nbsp;({description}){appeared ? " appeared on the network" : " disappeared from the network"}</h6>
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onDismiss} />
-    </div>;
-}
+import { Toast } from "../../components/Toast";
 
 interface DiscoveryMessage {
     type: string;
@@ -48,35 +33,37 @@ type DeviceListContainerProps = DataFetchProps<UpnpDevice[]> & TemplatedDataComp
 export class DeviceListContainer extends React.Component<DeviceListContainerProps, DeviceListState> {
 
     onDiscoveryEvent = (device: string, message: DiscoveryMessage) => {
-        this.showAlert(device, message);
         const reload = this.props.dataContext?.reload;
-        if (typeof reload === "function") reload();
+        if (typeof reload === "function") {
+            reload({ alerts: this.state.alerts.set(device, message) });
+        }
     }
 
     handlers: Map<string, SignalRMessageHandler> = new Map([["SsdpDiscoveryEvent", this.onDiscoveryEvent]]);
 
     state: DeviceListState = { alerts: new Map() };
 
-    showAlert = (device: string, message: DiscoveryMessage) => {
-        this.state.alerts.set(device, message);
-        this.setState({ alerts: this.state.alerts });
-    }
-
-    dismissAlert = (key: string) => {
-        this.state.alerts.delete(key);
-        this.setState({ alerts: this.state.alerts });
+    dismissAlert = (element: HTMLDivElement) => {
+        const key = element.dataset["id"];
+        if (key) {
+            this.state.alerts.delete(key);
+            this.setState({ alerts: this.state.alerts });
+        }
     }
 
     render() {
         const { dataContext, itemTemplate: Item, fetching, category } = this.props;
 
-        const alerts = Array.from(this.state.alerts).map(({ 0: key, 1: { type, info: { name, description } } }) =>
-            <DiscoveryAlert key={key} type={type} name={name} description={description} onDismiss={() => this.dismissAlert(key)} />);
-
         return fetching || !dataContext?.source
             ? <LoadIndicatorOverlay />
             : <SignalRListener handlers={this.handlers}>
-                {alerts}
+                {!fetching && this.state.alerts.size > 0 &&
+                    <div className="m-3 mb-0 d-flex flex-wrap justify-content-center gap-3">{
+                        Array.from(this.state.alerts).map(({ 0: key, 1: { type, info: { name, description } } }) =>
+                            <Toast key={key} data-id={key} header={name} color={type === "appeared" ? "success" : "warning"}
+                                delay={120000} onDismiss={this.dismissAlert}><b>&laquo;{description}&raquo;</b> has {type === "appeared" ? "appeared on" : "disappeared from"} the network</Toast>
+                        )}
+                    </div>}
                 <div className="d-grid grid-auto-x3 align-items-start justify-content-evenly m-3">
                     {[dataContext.source.map(item => <Item key={item.udn} data-source={item} category={category} />)]}
                 </div>
