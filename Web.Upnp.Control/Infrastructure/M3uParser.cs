674b4e74a42c6bcffa3166bcef035f200dd911ff
Lapiniot <aossss@gmail.com>
2020-12-25 18:08:48 +0200

    end of the stream handling bug fixed

--- Web.Upnp.Control/Infrastructure/M3uParser.cs
+++ Web.Upnp.Control/Infrastructure/M3uParser.cs
@@ -3,6 +3,7 @@ using System.Buffers;
 using System.Buffers.Text;
 using System.Collections.Generic;
 using System.IO.Pipelines;
+using System.Memory;
 using System.Text;
 using static System.Text.Encoding;
 
@@ -10,17 +11,18 @@ namespace Web.Upnp.Control.Infrastructure
 {
     public class M3uParser : ByteSequenceParser<(string Path, string Info, int Duration)>
     {
-        private static byte[] NewLine = new byte[] { 0x0d, 0x0a };
         private static byte[] EXTINF = new byte[] { 0x45, 0x58, 0x54, 0x49, 0x4E, 0x46, 0x3A };
 
         public M3uParser(PipeReader reader) : base(reader)
         {
         }
 
-        protected override IEnumerable<ParseResult> Parse(ReadOnlySequence<byte> buffer)
+        protected override IEnumerable<ParseResult> Parse(ReadResult result)
         {
             var consumed = 0L;
-            while(consumed < buffer.Length && TryReadTrack(buffer.Slice(consumed), out var line, out long c))
+            var buffer = result.Buffer;
+
+            while(consumed < buffer.Length && TryReadTrack(buffer.Slice(consumed), result.IsCompleted || result.IsCanceled, out var line, out long c))
             {
                 consumed += c;
                 if(line.Path is null) continue;
@@ -28,17 +30,15 @@ namespace Web.Upnp.Control.Infrastructure
             }
         }
 
-        private bool TryReadTrack(ReadOnlySequence<byte> buffer, out (string Path, string Info, int Duration) track, out long consumed)
+        private bool TryReadTrack(ReadOnlySequence<byte> buffer, bool isCompleted, out (string Path, string Info, int Duration) track, out long consumed)
         {
             consumed = 0;
             track = default;
 
             var lineReader = new SequenceReader<byte>(buffer);
 
-            while(lineReader.TryReadToAny(out ReadOnlySequence<byte> line, NewLine))
+            while(lineReader.TryReadLine(out ReadOnlySequence<byte> line, !isCompleted))
             {
-                lineReader.AdvancePastAny(0x0d, 0x0a);
-
                 if(line.Length == 0) continue;
 
                 var reader = new SequenceReader<byte>(line);
@@ -47,8 +47,7 @@ namespace Web.Upnp.Control.Infrastructure
                     if(!reader.IsNext(EXTINF, true)) continue;
                     reader.AdvancePast(0x20);
                     if(!(reader.TryReadTo(out ReadOnlySpan<byte> span, 0x2C) && Utf8Parser.TryParse(span, out int duration, out _))) continue;
-                    if(!lineReader.TryReadToAny(out line, NewLine)) break;
-                    lineReader.AdvancePastAny(0x0d, 0x0a);
+                    if(!lineReader.TryReadLine(out line, !isCompleted)) break;
                     track = (UTF8.GetString(line), UTF8.GetString(reader.UnreadSequence), duration);
                 }
                 else
