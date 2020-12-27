cdd479882c74b339bfdee3456e7a119f02b9c7be
Lapiniot <aossss@gmail.com>
2020-12-27 20:42:05 +0200

    LoadIndicator refactored

--- Web.Upnp.Control/ClientApp/src/components/LoadIndicator.tsx
+++ Web.Upnp.Control/ClientApp/src/components/LoadIndicator.tsx
@@ -1,11 +1,24 @@
 ﻿import React, { HTMLAttributes, PropsWithChildren } from "react";
 
-export default function LoadIndicator({ children = "Loading...", className, ...others }: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+export function Indicator({ children, className, ...others }: HTMLAttributes<HTMLDivElement>) {
     return <div className={`d-inline-flex justify-content-center align-items-center${className ? ` ${className}` : ""}`} {...others}>
+        {children}
+    </div>
+}
+
+export function LoadIndicator({ children = "Loading...", ...others }: HTMLAttributes<HTMLDivElement>) {
+    return <Indicator {...others}>
         <span className="visually-hidden" role="status">Loading...</span>
         <i className="fas fa-compact-disc fa-spin fa-3x" />
         {children}
-    </div>
+    </Indicator>
+}
+
+export function DropTargetIndicator({ children = "Drop files here", ...others }: HTMLAttributes<HTMLDivElement>) {
+    return <Indicator {...others}>
+        <i className="fas fa-upload fa-3x" />
+        {children}
+    </Indicator>
 }
 
 export function LoadIndicatorOverlay({ children }: PropsWithChildren<{}>) {
