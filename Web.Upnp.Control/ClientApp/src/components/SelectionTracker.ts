9d8f49d4aac0ab2b7e7c74085db9adc201e5f895
Lapiniot <aossss@gmail.com>
2021-01-09 18:51:50 +0200

    focus state fixes

--- Web.Upnp.Control/ClientApp/src/components/SelectionTracker.ts
+++ Web.Upnp.Control/ClientApp/src/components/SelectionTracker.ts
@@ -18,7 +18,7 @@ interface SelectionStore {
 export class SelectionTracker {
     private items: string[];
     private store: SelectionStore;
-    private focus: string | null = null;
+    private current: string | null = null;
     private onchanged: (ids: string[], hint: EventHint, handled: boolean) => void;
 
     constructor(selectables: string[], store: SelectionStore, onChanged: (ids: string[], hint: EventHint, handled: boolean) => void) {
@@ -36,11 +36,18 @@ export class SelectionTracker {
     }
 
     get focused() {
-        return this.focus;
+        return this.current;
     }
 
     blur() {
-        this.focus = null;
+        this.current = null;
+    }
+
+    clear(hint?: EventHint) {
+        if (this.enabled()) {
+            this.current = null;
+            this.onchanged([], hint ?? EventHint.None, this.store.clear());
+        }
     }
 
     setup(selectables?: string[], selection?: SelectionStore) {
@@ -62,13 +69,13 @@ export class SelectionTracker {
 
     selectNext(hint?: EventHint) {
         if (this.enabled()) {
-            this.selectOnly(this.focus ? this.items.indexOf(this.focus) + 1 : 0, hint);
+            this.selectOnly(this.current ? this.items.indexOf(this.current) + 1 : 0, hint);
         }
     }
 
     selectPrev(hint?: EventHint) {
         if (this.enabled()) {
-            this.selectOnly(this.focus ? this.items.indexOf(this.focus) - 1 : this.items.length - 1, hint)
+            this.selectOnly(this.current ? this.items.indexOf(this.current) - 1 : this.items.length - 1, hint)
         }
     }
 
@@ -76,72 +83,72 @@ export class SelectionTracker {
         if (this.enabled()) {
             if (this.store.one() && this.store.selected(id) === state) return;
             this.store.reset();
+            this.current = state ? id : null;
             this.onchanged([id], hint ?? EventHint.None, this.store.select(id, state));
-            this.focus = state ? id : null;
         }
     }
 
     set(id: string, state: boolean, hint?: EventHint) {
         if (this.enabled()) {
             if (this.store.one() && this.store.selected(id) === state) return;
+            this.current = state ? id : this.current;
             this.onchanged([id], hint ?? EventHint.None, this.store.select(id, state));
-            this.focus = state ? id : this.focus;
         }
     }
 
     toggle(id: string, hint?: EventHint) {
         if (this.enabled()) {
             const state = !this.store.selected(id);
+            this.current = state ? id : null;
             this.onchanged([id], hint ?? EventHint.None, this.store.select(id, state));
-            this.focus = state ? id : null;
         }
     }
 
     setAll(state: boolean, hint?: EventHint) {
         if (this.enabled()) {
+            this.current = state ? this.items[this.items.length - 1] : null;
             this.onchanged(this.items, hint ?? EventHint.None, state ? this.store.selectMany(this.items, true) : this.store.clear());
-            this.focus = state ? this.items[this.items.length - 1] : null;
         }
     }
 
     expandTo(id: string, hint?: EventHint) {
         if (this.enabled()) {
-            if (!this.focus) {
+            if (!this.current) {
                 this.set(id, true, hint);
                 return;
             }
 
-            const current = this.items.indexOf(this.focus);
+            const current = this.items.indexOf(this.current);
             const next = this.items.indexOf(id);
             const solidRange = this.findFirstInRange(false, Math.min(current, next), Math.max(current, next)) === undefined;
             const range = solidRange
                 ? (next > current ? this.items.slice(current, next) : this.items.slice(next + 1, current + 1))
                 : (next > current ? this.items.slice(current, next + 1) : this.items.slice(next, current + 1));
+            this.current = this.items[next];
             this.onchanged(range, hint ?? EventHint.None, this.store.selectMany(range, !solidRange));
-            this.focus = this.items[next];
         }
     }
 
     expandUp(hint?: EventHint) {
         if (this.enabled()) {
 
-            if (!this.focus) {
+            if (!this.current) {
                 this.selectLast(hint);
                 return;
             }
 
-            const index = this.items.indexOf(this.focus) - 1;
+            const index = this.items.indexOf(this.current) - 1;
             if (index >= 0) {
                 if (!this.store.selected(this.items[index])) {
                     // previouse item is not selected - select it and move focus to the beginning of a new "solid" range
                     const next = this.findLastInRange(false, 0, index - 1);
+                    this.current = this.items[next !== undefined ? next + 1 : 0];
                     this.select(index, true, hint);
-                    this.focus = this.items[next !== undefined ? next + 1 : 0];
                 }
                 else {
                     // previouse item is already selected - unselect it and move focus one item up, "trimming" nearest range from the bottom
+                    this.current = this.items[index];
                     this.select(index + 1, false, hint);
-                    this.focus = this.items[index];
                 }
             }
         }
@@ -150,23 +157,23 @@ export class SelectionTracker {
     expandDown(hint?: EventHint) {
         if (this.enabled()) {
 
-            if (!this.focus) {
+            if (!this.current) {
                 this.selectFirst(hint);
                 return;
             }
 
-            const index = this.items.indexOf(this.focus) + 1;
+            const index = this.items.indexOf(this.current) + 1;
             if (index < this.items.length) {
                 if (!this.store.selected(this.items[index])) {
                     // nex item is not selected - select it and move focus to the end of a new "solid" range
                     const next = this.findFirstInRange(false, index + 1, this.items.length);
+                    this.current = this.items[next !== undefined ? next - 1 : this.items.length - 1];
                     this.select(index, true, hint);
-                    this.focus = this.items[next !== undefined ? next - 1 : this.items.length - 1];
                 }
                 else {
                     // next item is already selected - unselect it and move focus one item down, "trimming" nearest range from the top
+                    this.current = this.items[index];
                     this.select(index - 1, false, hint);
-                    this.focus = this.items[index];
                 }
             }
         }
@@ -176,8 +183,8 @@ export class SelectionTracker {
         if (index >= 0 && index < this.items.length) {
             this.store.reset();
             const item = this.items[index];
+            this.current = item;
             this.onchanged([item], hint ?? EventHint.None, this.store.select(item, true));
-            this.focus = item;
         }
     }
 
@@ -185,7 +192,6 @@ export class SelectionTracker {
         if (index >= 0 && index < this.items.length) {
             const item = this.items[index];
             this.onchanged([item], hint ?? EventHint.None, this.store.select(item, state));
-            this.focus = item;
         }
     }
 
