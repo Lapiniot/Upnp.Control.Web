7994c8d9ed603f24537ae704e31e3db9c0b26537
Lapiniot <aossss@gmail.com>
2021-01-17 13:06:42 +0200

    PaginationSvgSymbols added with svg icons from Bootstrap Icons

--- Web.Upnp.Control/ClientApp/src/routes/common/SvgSymbols.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/SvgSymbols.tsx
@@ -28,3 +28,20 @@ export function PlaylistSvgSymbols() {
         <i data-fa-symbol="stop" className="fas fa-stop" />
     </>;
 }
+
+export function PaginationSvgSymbols() {
+    return <svg xmlns="http://www.w3.org/2000/svg" style={{ display: "none" }}>
+        <symbol id="chevron-bar-left" fill="currentColor" stroke="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M11.854 3.646a.5.5 0 0 1 0 .708L8.207 8l3.647 3.646a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708 0zM4.5 1a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 1 0v-13a.5.5 0 0 0-.5-.5z" />
+        </symbol>
+        <symbol id="chevron-bar-right" fill="currentColor" stroke="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M4.146 3.646a.5.5 0 0 0 0 .708L7.793 8l-3.647 3.646a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708 0zM11.5 1a.5.5 0 0 1 .5.5v13a.5.5 0 0 1-1 0v-13a.5.5 0 0 1 .5-.5z" />
+        </symbol>
+        <symbol id="chevron-left" fill="currentColor" stroke="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+        </symbol>
+        <symbol id="chevron-right" fill="currentColor" stroke="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+        </symbol>
+    </svg>;
+}
