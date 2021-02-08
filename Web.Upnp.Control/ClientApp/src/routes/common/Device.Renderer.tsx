ea7a2e1c9b3f432c86458f1c906fddffae7dc19e
Lapiniot <aossss@gmail.com>
2021-02-08 17:07:03 +0200

    OpenAction separated to its own file

--- Web.Upnp.Control/ClientApp/src/routes/common/Device.Renderer.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/Device.Renderer.tsx
@@ -1,5 +1,5 @@
-import { OpenMediaAction } from "./actions/Actions";
 import { AddBookmarkAction } from "./actions/AddBookmarkAction";
+import { OpenMediaAction } from "./actions/OpenAction";
 import { ActionDescriptor, DeviceCard } from "./DeviceCard";
 import PlayerWidget from "./PlayerWidget";
 import { DataSourceProps, UpnpDevice } from "./Types";
--- Web.Upnp.Control/ClientApp/src/routes/common/actions/Actions.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/actions/Actions.tsx
@@ -1,23 +1,11 @@
-import React, { HTMLAttributes, ReactNode } from "react";
+import React, { HTMLAttributes } from "react";
 import { NavLink, RouteLink } from "../../../components/NavLink";
-import WebApi from "../../../components/WebApi";
-import { BrowserProps, RowState } from "../BrowserView";
-import BrowserDialog, { BrowseResult } from "../BrowserDialog";
-import { DIDLUtils } from "../BrowserUtils";
 import { Services, UpnpDevice } from "../Types";
 
 export type DeviceActionProps = HTMLAttributes<HTMLElement> & {
     device: UpnpDevice;
     category?: string;
 };
-
-const browserProps: BrowserProps<unknown> = {
-    rowState: () => RowState.Navigable | RowState.Selectable
-}
-
-const audioBrowserProps: BrowserProps<unknown> = {
-    rowState: (item) => DIDLUtils.isMusicTrack(item) ? RowState.Selectable | RowState.Navigable : RowState.Navigable
-}
 
 export function BrowseContentAction({ device, category, className, ...other }: DeviceActionProps) {
     const isMediaServer = device.services && device.services.some(
@@ -29,48 +17,4 @@ export function BrowseContentAction({ device, category, className, ...other }: D
 
 export function DownloadMetadataAction({ device, category, className, ...other }: DeviceActionProps) {
     return <NavLink to={device.url} glyph="download" className={`py-0 px-1${className ? ` ${className}` : ""}`} {...other}>Metadata</NavLink>;
-}
-
-export class OpenAction extends React.Component<DeviceActionProps & { browserProps: BrowserProps<unknown> }, { modal?: ReactNode | null }>{
-    state = { modal: null }
-
-    resetModal = () => this.setState({ modal: null });
-
-    playMedia = (data: BrowseResult) => {
-        const deviceId = this.props.device.udn;
-        const { keys: { 0: objectId }, device: source } = data;
-        if (!deviceId || !source || !objectId) return;
-        return WebApi.control(deviceId).play(objectId, source).fetch();
-    }
-
-    browse = () => {
-        this.setState({
-            modal: <BrowserDialog id="open-media-dialog" title="Select media to play" confirmText="Open" className="modal-lg"
-                onDismissed={this.resetModal} onConfirmed={this.playMedia} browserProps={this.props.browserProps} immediate>
-            </BrowserDialog>
-        })
-    }
-
-    render() {
-        const { children, className, browserProps, device, category, ...other } = this.props;
-        return <>
-            <button type="button" className={`btn btn-round btn-plain${className ? ` ${className}` : ""}`} data-bs-toggle="dropdown"
-                aria-expanded="false" title="Browse for external media to play on this device" {...other} onClick={this.browse}>
-                {children}
-            </button>
-            {this.state.modal}
-        </>;
-    }
-}
-
-export function OpenMediaAction(props: DeviceActionProps) {
-    return <OpenAction {...props} browserProps={browserProps}>
-        <svg className="icon"><use href="#photo-video" /></svg>
-    </OpenAction>
-}
-
-export function OpenAudioAction(props: DeviceActionProps) {
-    return <OpenAction {...props} browserProps={audioBrowserProps}>
-        <svg className="icon"><use href="#folder-open" /></svg>
-    </OpenAction>
 }
