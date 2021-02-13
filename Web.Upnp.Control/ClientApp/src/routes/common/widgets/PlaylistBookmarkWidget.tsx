2413ffea70f5b0b2bbbc0d96e1a3a5c3b43c3ebb
Lapiniot <aossss@gmail.com>
2021-02-13 17:50:21 +0200

    style fixes for PlaylistBookmarWidget overflow

--- Web.Upnp.Control/ClientApp/src/routes/common/widgets/PlaylistBookmarkWidget.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/widgets/PlaylistBookmarkWidget.tsx
@@ -16,11 +16,12 @@ export default function ({ device, id, title, icon, deviceName }: PlaylistBookma
 
     return <div className="card shadow-sm">
         <div className="card-body d-flex align-items-center p-0">
-            <RouteLink to={`/umi/${device}/playlists/${id}`} className="d-flex align-items-center p-3 text-decoration-none">
+            <RouteLink to={`/umi/${device}/playlists/${id}`}
+                className="d-flex align-items-center p-3 text-decoration-none flex-shrink-1 flex-grow-1 overflow-hidden">
                 <AlbumArt albumArts={icon ? [icon] : undefined} itemClass="" className="me-3" />
-                <div className="d-flex flex-wrap overflow-hidden" title={`${title} on ${deviceName}`}>
-                    <h6 className="card-title text-truncate flex-basis-100">{title}</h6>
-                    <p className="card-subtitle text-truncate small">[on {deviceName}]</p>
+                <div className="d-flex flex-column overflow-hidden" title={`${title} on ${deviceName}`}>
+                    <h6 className="card-title text-truncate">{title}</h6>
+                    <p className="card-subtitle small text-truncate">[on {deviceName}]</p>
                 </div>
             </RouteLink>
             <button type="button" className="btn btn-round btn-plain m-1" title={`Play \u00AB${title}\u00BB on ${deviceName}`} onClick={clickHandler}>
