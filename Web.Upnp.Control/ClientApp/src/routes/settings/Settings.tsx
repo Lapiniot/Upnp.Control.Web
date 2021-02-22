6b460a8fb41a9537f6f81bbe7d3a4eb252ad7b6f
Lapiniot <aossss@gmail.com>
2021-02-22 16:31:36 +0200

    useDlnaProxy option UI added

--- Web.Upnp.Control/ClientApp/src/routes/settings/Settings.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/settings/Settings.tsx
@@ -22,6 +22,14 @@ function NumberEditor({ className, callback, ...other }: InputHTMLAttributes<HTM
         className={`form-control form-control-sm w-auto${className ? ` ${className}` : ""}`} />;
 }
 
+function FlagEditor({ className, callback, ...other }: InputHTMLAttributes<HTMLInputElement> & { callback: (value: boolean) => void }) {
+    const changedHandler = useCallback(({ target: { checked } }) => callback(checked), [callback]);
+    return <div className="form-check form-switch">
+        <input {...other} type="checkbox" onChange={changedHandler}
+            className={`form-check-input${className ? ` ${className}` : ""}`} />
+    </div>;
+}
+
 function setTimeout(timeout: number) {
     $s.set("timeout", timeout);
 }
@@ -34,39 +42,47 @@ function setScanDepth(depth: number) {
     $s.set("containerScanDepth", depth);
 }
 
+function setUseProxy(useProxy: boolean) {
+    $s.set("useDlnaProxy", useProxy);
+}
+
 export default () => {
-    return <div className="m-0 m-sm-3 d-flex flex-column w-md-50">
-        <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-                <small>General</small>
-                <div className="d-grid grid-1fr-auto gap-2 mt-1 align-items-center">
-                    <label htmlFor="page-size-select">Default page size</label>
-                    <PageSizeSelect id="page-size-select" />
-                    <label htmlFor="timeout-editor">Default request timeout (ms.)</label>
-                    <NumberEditor id="timeout-editor" min="1000" max="30000" step="200" defaultValue={$s.get("timeout")} callback={setTimeout} />
-                    <label htmlFor="scan-depth-editor">Container scan depth</label>
-                    <NumberEditor id="scan-depth-editor" min="0" max="5" defaultValue={$s.get("containerScanDepth")} callback={setScanDepth} />
-                    <label htmlFor="scan-timeout-editor">Container scan timeout (ms.)</label>
-                    <NumberEditor id="scan-timeout-editor" min="1000" max="90000" step="200" defaultValue={$s.get("containerScanTimeout")} callback={setScanTimeout} />
-                </div>
-            </li>
-            <li className="list-group-item">
-                <small>Tools</small>
-                <ul className="list-unstyled mt-1 d-flex flex-column flex-gapy-2">
-                    <li className="d-flex flex-column">
-                        <a className="btn-link" href="api/swagger" target="_blank" rel="noopener noreferrer">Open SwaggerUI</a>
-                        <small className="form-text">Offers a web-based UI that provides information about the service, using the generated OpenAPI specification</small>
-                    </li>
-                    <li className="d-flex flex-column">
-                        <a className="btn-link" href="api/cert">Download Certificate</a>
-                        <small className="form-text">You can obtain a copy of SSL certificate used by this site - this is especially useful if you need to add it manually to the trusted store on your device</small>
-                    </li>
-                    <li className="d-flex flex-column">
-                        <a className="btn-link" href="api/health">Server Health</a>
-                        <small className="form-text">Health checks are usually used with an external monitoring service or container orchestrator to check the status of an app</small>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+    return <div className="overflow-auto">
+        <div className="m-0 m-sm-3 d-flex flex-column w-md-50">
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                    <small>General</small>
+                    <div className="d-grid grid-1fr-auto gap-2 mt-1 align-items-center">
+                        <label htmlFor="page-size-select">Default page size</label>
+                        <PageSizeSelect id="page-size-select" />
+                        <label htmlFor="timeout-editor">Default request timeout (ms.)</label>
+                        <NumberEditor id="timeout-editor" min="1000" max="30000" step="200" defaultValue={$s.get("timeout")} callback={setTimeout} />
+                        <label htmlFor="scan-depth-editor">Container scan depth</label>
+                        <NumberEditor id="scan-depth-editor" min="0" max="5" defaultValue={$s.get("containerScanDepth")} callback={setScanDepth} />
+                        <label htmlFor="scan-timeout-editor">Container scan timeout (ms.)</label>
+                        <NumberEditor id="scan-timeout-editor" min="1000" max="90000" step="200" defaultValue={$s.get("containerScanTimeout")} callback={setScanTimeout} />
+                        <label htmlFor="use-proxy-editor">Use DLNA proxy</label>
+                        <FlagEditor id="use-proxy-editor" className="ms-auto" defaultChecked={$s.get("useDlnaProxy")} callback={setUseProxy} />
+                    </div>
+                </li>
+                <li className="list-group-item">
+                    <small>Tools</small>
+                    <div className="d-flex flex-column flex-gapy-2 mt-1">
+                        <div className="d-flex flex-column">
+                            <a className="btn-link" href="api/swagger" target="_blank" rel="noopener noreferrer">Open SwaggerUI</a>
+                            <small className="form-text">Offers a web-based UI that provides information about the service, using the generated OpenAPI specification</small>
+                        </div>
+                        <div className="d-flex flex-column">
+                            <a className="btn-link" href="api/cert">Download Certificate</a>
+                            <small className="form-text">You can obtain a copy of SSL certificate used by this site - this is especially useful if you need to add it manually to the trusted store on your device</small>
+                        </div>
+                        <div className="d-flex flex-column">
+                            <a className="btn-link" href="api/health" rel="noopener noreferrer">Server Health</a>
+                            <small className="form-text">Health checks are usually used with an external monitoring service or container orchestrator to check the status of an app</small>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 }
