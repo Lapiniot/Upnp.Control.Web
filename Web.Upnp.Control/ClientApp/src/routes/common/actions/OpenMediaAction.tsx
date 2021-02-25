837f589e5f27626e99e38fdb72415e7c0e9bc177
Lapiniot <aossss@gmail.com>
2021-02-25 14:17:53 +0200

    OpenMediaAction: 1. Transformed to more compact functional component form 2. Reworked to use ModalHost for modal UI presentation

--- Web.Upnp.Control/ClientApp/src/routes/common/actions/OpenMediaAction.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/actions/OpenMediaAction.tsx
@@ -1,10 +1,10 @@
-import React, { ReactNode } from "react";
+import { useCallback, useRef } from "react";
 import WebApi from "../../../components/WebApi";
 import { BrowserProps, RowState } from "../BrowserView";
 import BrowserDialog, { BrowseResult } from "../BrowserDialog";
 import { DeviceActionProps } from "./Actions";
 import { DIDLUtils } from "../BrowserUtils";
-import { Portal } from "../../../components/Portal";
+import ModalHost from "../../../components/ModalHost";
 
 const browserProps: BrowserProps<unknown> = {
     rowState: () => RowState.Navigable | RowState.Selectable
@@ -14,39 +14,27 @@ const audioBrowserProps: BrowserProps<unknown> = {
     rowState: (item) => DIDLUtils.isMusicTrack(item) ? RowState.Selectable | RowState.Navigable : RowState.Navigable
 }
 
-export class OpenAction extends React.Component<DeviceActionProps & { browserProps: BrowserProps<unknown>; }, { modal?: ReactNode | null; }> {
-    state = { modal: null };
-
-    resetModal = () => this.setState({ modal: null });
-
-    playMedia = (data: BrowseResult) => {
-        const deviceId = this.props.device.udn;
+export function OpenAction({ children, className, browserProps, device, category, ...other }: DeviceActionProps & { browserProps: BrowserProps<unknown> }) {
+    const modalHostRef = useRef<ModalHost>(null);
+    const playHandler = useCallback((data: BrowseResult) => {
+        const deviceId = device.udn;
         const { keys: { 0: objectId }, device: source } = data;
-        if (!deviceId || !source || !objectId)
-            return;
+        if (!deviceId || !source || !objectId) return;
         return WebApi.control(deviceId).play(objectId, source).fetch();
-    };
-
-    browse = () => {
-        this.setState({
-            modal: <BrowserDialog className="modal-lg modal-fullscreen-sm-down" title="Select media to play" confirmText="Open"
-                onDismissed={this.resetModal} onConfirmed={this.playMedia} browserProps={this.props.browserProps} immediate>
-            </BrowserDialog>
-        });
-    };
-
-    render() {
-        const { children, className, browserProps, device, category, ...other } = this.props;
-        return <>
-            <button type="button" className={`btn btn-round btn-plain${className ? ` ${className}` : ""}`} data-bs-toggle="dropdown"
-                aria-expanded="false" title="Browse for external media to play on this device" {...other} onClick={this.browse}>
-                {children}
-            </button>
-            <Portal selector="#modal-root">
-                {this.state.modal}
-            </Portal>
-        </>;
-    }
+    }, [device]);
+    const browseClickHandler = useCallback(() =>
+        modalHostRef.current?.show(
+            <BrowserDialog className="modal-lg modal-fullscreen-sm-down" title="Select media to play" confirmText="Open"
+                onConfirmed={playHandler} browserProps={browserProps}>
+            </BrowserDialog>)
+        , [device]);
+    return <>
+        <button type="button" className={`btn btn-round btn-plain${className ? ` ${className}` : ""}`} data-bs-toggle="dropdown"
+            aria-expanded="false" title="Browse for external media to play on this device" {...other} onClick={browseClickHandler}>
+            {children}
+        </button>
+        <ModalHost ref={modalHostRef} />
+    </>;
 }
 
 export function OpenMediaAction(props: DeviceActionProps) {
