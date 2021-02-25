80cb82271ede7f156498f5430ccae78ae5ea71fb
Lapiniot <aossss@gmail.com>
2021-02-25 11:50:22 +0200

    empty Portal should not render

--- Web.Upnp.Control/ClientApp/src/components/Portal.tsx
+++ Web.Upnp.Control/ClientApp/src/components/Portal.tsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 
 export function Portal({ children, selector }: PropsWithChildren<{ selector: string; }>) {
     const container = document.querySelector<HTMLElement>(selector);
-    return container ? ReactDOM.createPortal(children, container) : null;
+    return container && children ? ReactDOM.createPortal(children, container) : null;
 }
