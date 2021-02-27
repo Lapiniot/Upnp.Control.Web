f0dd9de9d7ba8bcd655e1e13b77a4bf0c43a09ff
Lapiniot <aossss@gmail.com>
2021-02-27 11:32:41 +0200

    bottom bar has white background by default

--- Web.Upnp.Control/ClientApp/src/routes/common/BottomBar.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/BottomBar.tsx
@@ -2,6 +2,6 @@ import { HTMLAttributes } from "react";
 
 export function BottomBar({ className, style = {}, ...other }: HTMLAttributes<HTMLDivElement>) {
     style.bottom = "-1px";
-    const defaultClass = "sticky-bottom d-flex align-items-center justify-content-end bg-light border-top p-1";
+    const defaultClass = "sticky-bottom d-flex align-items-center justify-content-end bg-white border-top p-1 px-2";
     return <div {...other} style={style} className={`${defaultClass}${className ? ` ${className}` : ""}`} />;
 }
