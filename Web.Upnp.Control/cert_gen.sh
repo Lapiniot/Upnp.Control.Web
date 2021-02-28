c3ba89fca11e60eb48745dc4e479be808b0d3f73
Lapiniot <aossss@gmail.com>
2021-02-28 19:18:01 +0200

    cert_gen.sh script improved to include all mandatory extensions needed by modern OS and browsers

--- Web.Upnp.Control/cert_gen.sh
+++ Web.Upnp.Control/cert_gen.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-apt update
-apt install openssl
-apt install ca-certificates
-
 PARENT=$(hostname)
 FILE_NAME=upnp-dashboard
-USER=upnp-dashboard
-GROUP=upnp-dashboard
 
 openssl req \
 -x509 \
@@ -17,37 +11,30 @@ openssl req \
 -nodes \
 -keyout $FILE_NAME.key \
 -out $FILE_NAME.crt \
--subj "/CN=${PARENT}" \
--extensions v3_ca \
 -extensions v3_req \
 -config <( \
-  echo '[req]'; \
-  echo 'default_bits= 4096'; \
-  echo 'distinguished_name=req'; \
-  echo 'x509_extension = v3_ca'; \
-  echo 'req_extensions = v3_req'; \
-  echo '[v3_req]'; \
-  echo 'basicConstraints = CA:FALSE'; \
-  echo 'keyUsage = nonRepudiation, digitalSignature, keyEncipherment'; \
-  echo 'subjectAltName = @alt_names'; \
-  echo '[ alt_names ]'; \
-  echo "DNS.1 = www.${PARENT}"; \
-  echo "DNS.2 = ${PARENT}"; \
-  echo "DNS.3 = localhost"; \
-  echo "DNS.4 = 127.0.0.1"; \
-  echo '[ v3_ca ]'; \
-  echo 'subjectKeyIdentifier=hash'; \
-  echo 'authorityKeyIdentifier=keyid:always,issuer'; \
-  echo 'basicConstraints = critical, CA:TRUE, pathlen:0'; \
-  echo 'keyUsage = critical, cRLSign, keyCertSign'; \
-  echo 'extendedKeyUsage = serverAuth, clientAuth')
+  echo "[req]"; \
+  echo "default_bits            = 4096"; \
+  echo "distinguished_name      = req_distinguished_name"; \
+  echo "x509_extensions         = v3_req"; \
+  echo "req_extensions          = v3_req"; \
+  echo "prompt                  = no"; \
+  echo "[req_distinguished_name]"; \
+  echo "commonName              = ${PARENT}"; \
+  echo "[v3_req]"; \
+  echo "basicConstraints        = critical, CA:FALSE"; \
+  echo "keyUsage                = critical, nonRepudiation, keyCertSign, cRLSign, digitalSignature, keyEncipherment"; \
+  echo "extendedKeyUsage        = critical, serverAuth"; \
+  echo "subjectAltName          = critical, @alt_names"; \
+  echo "subjectKeyIdentifier    = hash"; \
+  echo "authorityKeyIdentifier  = keyid:always, issuer"; \
+  echo "1.3.6.1.4.1.311.84.1.1  = ASN1:UTF8String:02"; \
+  echo "[alt_names]"; \
+  echo "DNS.1                   = www.${PARENT}"; \
+  echo "DNS.2                   = ${PARENT}"; \
+  echo "DNS.3                   = localhost"; \
+  echo "IP.1                    = 127.0.0.1")
 
 openssl x509 -noout -text -in $FILE_NAME.crt
+
 openssl pkcs12 -export -out $FILE_NAME.pfx -inkey $FILE_NAME.key -in $FILE_NAME.crt
-
-#cp --force --verbose ./$FILE_NAME.crt /usr/local/share/ca-certificates/
-
-#update-ca-certificates --verbose
-
-#chown $USER:$GROUP $FILE_NAME.crt $FILE_NAME.key
-#chmod 0444 $FILE_NAME.crt $FILE_NAME.key
