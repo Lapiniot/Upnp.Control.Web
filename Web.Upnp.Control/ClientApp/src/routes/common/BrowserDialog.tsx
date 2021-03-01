6fd8d70edb772c3e9b100cbcce5823fe65c3a197
Lapiniot <aossss@gmail.com>
2021-03-01 12:30:33 +0200

    fixes for Safari quirks taken into account

--- Web.Upnp.Control/ClientApp/src/routes/common/BrowserDialog.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/BrowserDialog.tsx
@@ -28,17 +28,16 @@ export type BrowseResult = {
 const serversFetch = $api.devices("servers").jsonFetch;
 
 const MediaSourceList = withDataFetch(({ dataContext: ctx, fetching }: DataFetchProps<UpnpDevice[]>) =>
-    <div>
-        {fetching
-            ? <LoadIndicatorOverlay />
-            : <ul className="list-group list-group-flush border-top border-bottom">
-                {ctx?.source?.map(({ udn, name, type, description, icons }) =>
-                    <RouteLink key={udn} to={`/sources/${udn}`} className="nav-link list-group-item list-group-item-action d-flex align-items-center">
-                        <DeviceIcon icons={icons} service={type} />
-                        {name}{description && ` (${description})`}
-                    </RouteLink>)}
-            </ul>}
-    </div>, () => serversFetch, { usePreloader: true });
+    fetching
+        ? <LoadIndicatorOverlay />
+        : <ul className="list-group list-group-flush">
+            {ctx?.source?.map(({ udn, name, type, description, icons }) =>
+                <RouteLink key={udn} to={`/sources/${udn}`} className="nav-link list-group-item list-group-item-action d-flex align-items-center">
+                    <DeviceIcon icons={icons} service={type} />
+                    {name}{description && ` (${description})`}
+                </RouteLink>)}
+        </ul>
+    , () => serversFetch, { usePreloader: true });
 
 const Browser = withBrowserDataFetch(BrowserCore, false);
 
@@ -68,14 +67,14 @@ export default class BrowserDialog extends React.Component<BrowserDialogProps, {
     render() {
         const { title, confirmContent = "Open", onConfirmed, browserProps = {}, selection, ...other } = this.props;
         return <Modal title={title} {...other} data-bs-keyboard={true} ref={this.modalRef}>
-            <Modal.Body className="overflow-hidden p-0 position-relative d-flex flex-column" style={{ height: "60vh" }}>
-                <div className="overflow-auto flex-grow-1 d-flex flex-column">
+            <Modal.Body className="overflow-hidden p-0 position-relative d-flex flex-column border-bottom border-top" style={{ height: "60vh" }}>
+                <div className="overflow-auto flex-fill d-flex flex-column">
                     <MemoryRouter initialEntries={["/sources"]} initialIndex={0}>
                         <Switch>
                             <Route path={["/sources"]} exact render={() => <MediaSourceList />} />
                             <Route path={"/sources/:device/-1"} exact render={() => <Redirect to="/sources" />} />
                             <Route path={"/sources/:device/:id(.*)?"} render={props =>
-                                <Browser {...props} ref={this.browserRef} className="flex-fill border-top border-bottom"
+                                <Browser {...props} ref={this.browserRef}
                                     open={this.props.dismissOnOpen ? this.openHandler : undefined} {...browserProps}
                                     selectionChanged={this.selectionChanged} modalDialogMode />} />
                         </Switch>
