25642ce20d4083737cda168c219ec92c6052410d
Lapiniot <aossss@gmail.com>
2021-04-27 13:46:53 +0300

    SSL certificates generation on container startup

---
+++ Web.Upnp.Control/scripts/container-entrypoints.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ ! -f config/upnp-dashboard.pfx ]]; then
+    cd config
+    ../scripts/cert-gen.sh crupnppw
+    cd ..
+fi
+
+if [[ -f config/upnp-dashboard.pfx && ! -f config/appsettings.Production.Https.json ]]; then
+    cp .appsettings.Production.Https.json config/appsettings.Production.Https.json
+fi
+
+export ASPNETCORE_ENVIRONMENT=Production \
+    ASPNETCORE_Kestrel__Certificates__Default__Path=config/upnp-dashboard.pfx \
+    ASPNETCORE_Kestrel__Certificates__Default__Password=crupnppw
+
+exec ./Web.Upnp.Control
