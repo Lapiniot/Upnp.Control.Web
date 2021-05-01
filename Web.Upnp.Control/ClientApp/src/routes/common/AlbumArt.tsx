096abac505641568b029f004202f8128af751995
Lapiniot <aossss@gmail.com>
2021-05-01 14:14:25 +0300

    fixed: error on home page if itemClass is undefined

--- Web.Upnp.Control/ClientApp/src/routes/common/AlbumArt.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/AlbumArt.tsx
@@ -18,7 +18,9 @@ export function getIconByClass(itemClass: string) {
 
 type AlbumArtProps = HTMLAttributes<HTMLOrSVGElement> & { itemClass: string, albumArts?: string[] };
 
-export default ({ itemClass, albumArts, className, ...other }: AlbumArtProps) => {
+AlbumArt.defaultProps = { itemClass: "object.item" }
+
+export default function AlbumArt({ itemClass, albumArts, className, ...other }: AlbumArtProps) {
     return albumArts?.length
         ? <img src={viaProxy(albumArts[0])} className={`album-art${className ? ` ${className}` : ""}`} alt="" {...other} />
         : <svg className={`album-art${className ? ` ${className}` : ""}`} {...other}>
