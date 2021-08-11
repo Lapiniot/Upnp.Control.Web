e58ca8e86b949ad5718c474d8a0c5e89d82be288
Lapiniot <aossss@gmail.com>
2021-08-11 17:18:42 +0300

    NavigationProvider component draft which makes use of React context API

--- Web.Upnp.Control/ClientApp/src/routes/common/Navigator.tsx
+++ Web.Upnp.Control/ClientApp/src/routes/common/Navigator.tsx
@@ -1,7 +1,38 @@
-import { ComponentType, useCallback, useRef } from "react";
+import React, { ComponentType, PropsWithChildren, useCallback, useContext, useRef } from "react";
 import { generatePath, useHistory, useLocation, useRouteMatch } from "react-router";
 
-export type NavigatorProps = { navigate: (data: { [key: string]: string }, pattern?: string) => void }
+type NavigationHandler = (data: { [key: string]: string; }, pattern?: string) => void;
+export type NavigationContextState = { navigate: NavigationHandler, params: { [K: string]: string | undefined } };
+
+const NavigationContext = React.createContext<NavigationContextState>({ navigate: () => { }, params: {} });
+
+export { NavigationContext };
+
+export function NavigationProvider(props: PropsWithChildren<{}>) {
+    const { search } = useLocation();
+    const match = useRouteMatch();
+    const history = useHistory();
+
+    const ref = useRef({ history, match });
+    ref.current = { history, match };
+
+    const params: { [K: string]: any } = { ...match.params };
+    new window.URLSearchParams(search).forEach((value, key) => params[key] = value);
+
+    const navigate = useCallback((data, pattern) => {
+        const { history, match: { path, params } } = ref.current;
+        history.push(generatePath(pattern ?? path, { ...params, ...data }));
+    }, []);
+
+    return <NavigationContext.Provider value={{ navigate, params }}>{props.children}</NavigationContext.Provider>
+}
+
+export function useNavigation() {
+    const { navigate, params } = useContext(NavigationContext);
+    return { navigate, params };
+}
+
+export type NavigatorProps = { navigate: NavigationHandler }
 
 type InjectedProps<Params> = NavigatorProps & Params;
 
