9242a44d31ccd8439cead3cd8c81bd5349abee30
Lapiniot <aossss@gmail.com>
2021-10-14 16:33:48 +0300

    Timer component: efficient timer implementation has been separated into custom reusable useIntervalUpdate hook

---
+++ Web.Upnp.Control/ClientApp/src/components/IntervalUpdate.tsx
@@ -0,0 +1,33 @@
+// more efficient Timer implementation - inspired by https://www.youtube.com/watch?v=MCi6AZMkxcU
+import { useCallback, useEffect, useRef } from "react";
+
+function scheduleNext(start: number, current: number, interval: number, callback: FrameRequestCallback) {
+    const ellapsed = current - start;
+    const target = start + Math.round(ellapsed / interval) * interval + interval;
+    setTimeout(() => requestAnimationFrame(callback), target - performance.now());
+}
+
+type UpdateCallback = (ellapsed: number) => void;
+
+export function useIntervalUpdate(callback: UpdateCallback, active = true, interval = 1000) {
+    const props = useRef({ start: 0, interval, controller: null as (AbortController | null), callback });
+
+    const update = useCallback(time => {
+        const { start, interval, controller, callback } = props.current;
+        if (controller?.signal.aborted) return;
+        callback(time - start);
+        scheduleNext(start, time, interval, update);
+    }, []);
+
+    useEffect(() => {
+        props.current.controller?.abort();
+
+        if (active) {
+            const start = document.timeline.currentTime ?? 0;
+            props.current = { start, interval, controller: new AbortController(), callback };
+            scheduleNext(start, start, interval, update);
+        }
+
+        return () => props.current.controller?.abort();
+    }, [active, interval, callback]);
+}
--- Web.Upnp.Control/ClientApp/src/components/Timer.tsx
+++ Web.Upnp.Control/ClientApp/src/components/Timer.tsx
@@ -1,59 +1,17 @@
-import React, { HTMLAttributes, RefObject } from "react";
+import { HTMLAttributes, useCallback, useRef } from "react";
 import { formatTime } from "./Extensions";
+import { useIntervalUpdate } from "./IntervalUpdate";
 
 type TimerProps = { running: boolean; current: number; interval?: number }
 
-export default class extends React.Component<TimerProps & HTMLAttributes<HTMLTimeElement>> {
-    nativeRef = React.createRef<HTMLElement>();
-    start: number = 0;
-    controller: AbortController | null = null;
-    interval: number = 1000;
-
-    componentDidMount() {
-        if (this.props.running && Number.isFinite(this.props.current))
-            this.startTimer();
-    }
-
-    componentDidUpdate() {
-        this.stopTimer();
-        if (this.props.running && Number.isFinite(this.props.current)) {
-            this.startTimer();
+export default function ({ current, running, interval, ...other }: TimerProps & HTMLAttributes<HTMLTimeElement>) {
+    const ref = useRef<HTMLElement>(null);
+    const callback = useCallback((ellapsed) => {
+        if (ref?.current) {
+            ref.current.textContent = formatTime(ellapsed / 1000 + current);
         }
-    }
+    }, [current]);
+    useIntervalUpdate(callback, running, interval);
 
-    componentWillUnmount() {
-        this.stopTimer();
-    }
-
-    startTimer = () => {
-        this.controller = new AbortController();
-        this.start = document.timeline.currentTime ?? 0;
-        this.interval = this.props.interval ?? 1000;
-        this.scheduleNext(this.start);
-    }
-
-    stopTimer = () => {
-        this.controller?.abort();
-    }
-
-    scheduleNext = (time: number) => {
-        const ellapsed = time - this.start;
-        const target = this.start + Math.round(ellapsed / this.interval) * this.interval + this.interval;
-        setTimeout(() => requestAnimationFrame(this.update), target - performance.now());
-    }
-
-    update = (time: number) => {
-        if (this.controller?.signal.aborted) return;
-
-        if (this.nativeRef?.current) {
-            this.nativeRef.current.textContent = formatTime((time - this.start) / 1000 + this.props.current);
-        }
-
-        this.scheduleNext(time);
-    }
-
-    render() {
-        const { current, running, ...other } = this.props;
-        return <time {...other} ref={this.nativeRef}>{formatTime(current)}</time>;
-    }
+    return <time {...other} ref={ref}>{formatTime(current)}</time>;
 }
