394381df4262198c3edf181b20863b7d610fe264
Lapiniot <aossss@gmail.com>
2022-06-14 15:50:28 +0300

    settings page layout fixes

--- Upnp.Control.Web/ClientApp/src/routes/settings/Settings.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/settings/Settings.tsx
@@ -40,7 +40,7 @@ export default () => {
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">
                     <small>General</small>
-                    <div className="d-grid grid-1fr-auto gap-3 mt-1 align-items-center">
+                    <div className="d-grid grid-1fr-auto gap-3 mt-2 align-items-center">
                         <label htmlFor="page-size-select">Default page size</label>
                         <OptionsEditor id="page-size-select" options={$s.get("pageSizes")} value={$s.get("pageSize")} callback={setPageSize} />
                         <label htmlFor="timeout-editor">Default request timeout (ms.)</label>
@@ -55,7 +55,7 @@ export default () => {
                 </li>
                 <li className="list-group-item">
                     <small>In-app notifications</small>
-                    <div className="d-grid grid-1fr-auto gap-3 mt-1 align-items-center">
+                    <div className="d-grid grid-1fr-auto gap-3 mt-2 align-items-center">
                         <label htmlFor="discovery-nt">Device discovery</label>
                         <FlagEditor id="discovery-nt" className="ms-auto" checked={$s.get("showDiscoveryNotifications")} callback={setShowDiscoveryNotifications} />
                         <label htmlFor="playback-nt">Playback state changes</label>
@@ -65,7 +65,7 @@ export default () => {
                 {("serviceWorker" in navigator) && ("PushManager" in window) && <>
                     <li className="list-group-item">
                         <small>Push notifications</small>
-                        <div className="d-grid grid-1fr-auto gap-3 mt-1 align-items-center">
+                        <div className="d-grid grid-1fr-auto gap-3 mt-2 align-items-center">
                             <label htmlFor="discovery-pushes-editor">Device discovery</label>
                             <PushSubscriptionToggle notificationType={NotificationType.DeviceDiscovery} id="discovery-pushes-editor" className="ms-auto" />
                             <label htmlFor="playback-pushes-editor">Playback state changes</label>
@@ -75,17 +75,13 @@ export default () => {
                 </>}
                 <li className="list-group-item">
                     <small>Tools</small>
-                    <div className="vstack mt-1 gap-2">
+                    <div className="vstack mt-2 gap-2">
                         <div className="vstack">
                             <a className="btn-link" href="api/swagger" target="_blank" rel="noopener noreferrer">Open SwaggerUI</a>
                             <small className="form-text">Offers a web-based UI that provides information about the service, using the generated OpenAPI specification</small>
-                        </div>
-                        <div className="vstack">
-                            <a className="btn-link" href="api/cert">Download Certificate(s)</a>
+                            <a className="btn-link mt-1" href="api/cert">Download Certificate(s)</a>
                             <small className="form-text">You can obtain a copy of SSL certificate used by this site - this is especially useful if you need to add it manually to the trusted store on your device</small>
-                        </div>
-                        <div className="vstack">
-                            <a className="btn-link" href="api/health" rel="noopener noreferrer">Server Health</a>
+                            <a className="btn-link mt-1" href="api/health" rel="noopener noreferrer">Server Health</a>
                             <small className="form-text">Health checks are usually used with an external monitoring service or container orchestrator to check the status of an app</small>
                         </div>
                     </div>
