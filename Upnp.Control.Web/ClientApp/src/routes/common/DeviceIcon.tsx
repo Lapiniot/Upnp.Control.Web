027f45e320ee03c13814d679606edc2fa9d1986c
Lapiniot <aossss@gmail.com>
2022-06-17 23:22:16 +0300

    icon related styles refactoring

--- Upnp.Control.Web/ClientApp/src/routes/common/DeviceIcon.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/DeviceIcon.tsx
@@ -8,5 +8,5 @@ export default ({ device: { icons, type } }: { device: UpnpDevice }) => {
     const attr: ImgHTMLAttributes<HTMLImageElement> = icon
         ? { src: viaProxy(icon.url) }
         : { src: `icons.svg#${UDT.getSpecialRole(type)}`, style: { objectFit: "unset" } };
-    return <img {...attr} className="upnp-dev-icon me-2" alt="" />;
+    return <img {...attr} className="icon icon-3x me-2" alt="" />;
 }
