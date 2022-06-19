71fabc528475b74df163d2baf04510f4bbcad906
Lapiniot <aossss@gmail.com>
2022-06-20 00:37:46 +0300

    Spoiler component button background removed

--- Upnp.Control.Web/ClientApp/src/components/Spoiler.tsx
+++ Upnp.Control.Web/ClientApp/src/components/Spoiler.tsx
@@ -3,7 +3,7 @@ import "bootstrap/js/src/collapse";
 
 export default ({ uniqueId, title, children, className, ...other }: { uniqueId: string; title: string; } & HTMLAttributes<HTMLDivElement>) =>
     <div className={`spoiler${className ? ` ${className}` : ""}`} {...other}>
-        <button type="button" className="btn btn-light collapsed w-100"
+        <button type="button" className="btn collapsed w-100"
             data-bs-toggle="collapse" data-bs-target={`#${uniqueId}`}
             aria-controls={uniqueId} aria-expanded="false">
             {title}
