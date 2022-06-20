0ba0a936ba36d18fc8a0ed3da42d64ea1e200d07
Lapiniot <aossss@gmail.com>
2022-06-20 11:51:29 +0300

    fix for incorrect svg icons scaling when rendered by img element

--- Upnp.Control.Web/ClientApp/src/routes/common/DeviceIcon.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/DeviceIcon.tsx
@@ -1,12 +1,8 @@
-import { ImgHTMLAttributes } from "react";
 import { viaProxy } from "../../components/Extensions";
 import { UpnpDevice } from "./Types";
 import { UpnpDeviceTools as UDT } from "./UpnpDeviceTools";
 
 export default ({ device: { icons, type } }: { device: UpnpDevice }) => {
     const icon = UDT.getOptimalIcon(icons);
-    const attr: ImgHTMLAttributes<HTMLImageElement> = icon
-        ? { src: viaProxy(icon.url) }
-        : { src: `icons.svg#${UDT.getSpecialRole(type)}`, style: { objectFit: "unset" } };
-    return <img {...attr} className="icon icon-3x me-2" alt="" />;
+    return <img src={icon ? viaProxy(icon.url) : `icons.svg#${UDT.getSpecialRole(type)}`} className="icon icon-3x me-2" alt="" />;
 }
