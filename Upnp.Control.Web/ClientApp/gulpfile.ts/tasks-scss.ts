7b26a79d245d3fc19252793e405438a176fe5d64
Lapiniot <aossss@gmail.com>
2022-06-30 11:08:37 +0300

    Material Icons symbol icons added

--- Upnp.Control.Web/ClientApp/gulpfile.ts/tasks-scss.ts
+++ Upnp.Control.Web/ClientApp/gulpfile.ts/tasks-scss.ts
@@ -7,14 +7,14 @@ const sources = ["src/styles/*.scss"];
 const includePaths = ["node_modules"];
 const destination = "src/css";
 
-gulp.task("sass", cb => {
+gulp.task("scss", cb => {
     gulp.src(sources)
         .pipe(sass({ includePaths }).on('error', sass.logError))
         .pipe(gulp.dest(destination));
     cb();
 });
 
-gulp.task("watch", cb => {
-    gulp.watch(sources, gulp.series("sass"));
+gulp.task("watch-scss", cb => {
+    gulp.watch(sources, gulp.series("scss"));
     cb();
 });
--- Upnp.Control.Web/ClientApp/gulpfile.ts/tasks-svg.ts
+++ Upnp.Control.Web/ClientApp/gulpfile.ts/tasks-svg.ts
@@ -1,13 +1,14 @@
 import gulp from "gulp";
 import merge from "./gulp-merge-svg";
+import spriteIcons from "./symbols-icons.json";
+import stackSvgs from "./stack-icons.json";
+
+type IconType = "" | "outlined" | "round" | "sharp" | "twotone";
+type IconImportConfig = (string | [name: string, type: string])[];
 
 const sourceFolder = "../../../material-design-icons/src";
 const destFolder = "./public";
-const iconType = "";
-const spriteSvgs = ["action/home", "action/settings", "device/devices", "device/storage",
-    "hardware/connected_tv", "hardware/speaker",
-    "av/play_circle", "av/stop_circle", "av/pause_circle"];
-const stackSvgs = ["device/storage", "hardware/connected_tv", "hardware/speaker"];
+const iconType: IconType = "outlined";
 
 function generateId(name: string) {
     const end = name.lastIndexOf('/', name.lastIndexOf('/') - 1);
@@ -15,24 +16,28 @@ function generateId(name: string) {
     return name.substring(start, end);
 }
 
-function getGlobs(svgs: string[]) {
-    return svgs.map(s => `${s}/materialicons${iconType}/24px.svg`);
+function getGlobs(config: IconImportConfig) {
+    return config.map(s => typeof s === "string"
+        ? `${s}/materialicons${iconType}/24px.svg`
+        : `${s[1]}/materialicons${s[0]}/24px.svg`);
 }
 
-gulp.task("svg", function buildSvg(done) {
-    gulp.src(getGlobs(spriteSvgs), { cwd: sourceFolder, cwdbase: true })
+gulp.task("svg-symbols", function buildSvg(done) {
+    gulp.src(getGlobs(spriteIcons), { cwd: sourceFolder, cwdbase: true })
         .pipe(merge({
             mode: "symbols", generateId,
             optimizations: ["promoteGroupChildren", "removeInvisible"]
         }))
         .pipe(gulp.dest(destFolder));
+    done();
+});
 
+gulp.task("svg-stack", function buildSvg(done) {
     gulp.src(getGlobs(stackSvgs), { cwd: sourceFolder, cwdbase: true })
         .pipe(merge({
             mode: "stack", generateId, dimensions: { w: 24, h: 24 },
             optimizations: ["promoteGroupChildren", "removeInvisible"]
         }))
         .pipe(gulp.dest(destFolder));
-
     done();
 });
