1ebaa5340651117f0fc5b2dea7db3b6a8226fa7a
Lapiniot <aossss@gmail.com>
2022-07-01 15:32:12 +0300

    AlbumArt component: icon name resolution optimized

--- Upnp.Control.Web/ClientApp/src/routes/common/AlbumArt.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/AlbumArt.tsx
@@ -3,47 +3,38 @@ import { viaProxy } from "../../components/Extensions";
 
 type ItemClassIcon = "folder" | "audio_file" | "video_file" | "insert_drive_file" | "insert_photo" | "album" | "queue_music" | "audiotrack" | "movie";
 
-function getIconClassic(itemClass: string): ItemClassIcon {
-    if (itemClass.startsWith("object.container")) {
-        if (itemClass.endsWith(".playlistContainer"))
-            return "queue_music"
-        else
-            return "folder";
-    } else if (itemClass.includes(".audioItem"))
-        return "audio_file";
-    else if (itemClass.includes(".videoItem"))
-        return "video_file";
-    else if (itemClass.includes(".imageItem"))
-        return "insert_photo";
-    else
-        return "insert_drive_file";
+type IconMap = {
+    [K in string]: ItemClassIcon;
+};
+
+const classicMap: IconMap = {
+    "container.storageFolder": "folder",
+    "container.playlistContainer": "queue_music",
+    "item.audioItem": "audio_file",
+    "item.audioItem.musicTrack": "audio_file",
+    "item.videoItem": "video_file",
+    "item.videoItem.movie": "video_file",
+    "item.imageItem": "insert_photo",
+    "item.imageItem.photo": "insert_photo"
 }
 
-function getIconPlayer(itemClass: string): ItemClassIcon {
-    if (itemClass.startsWith("object.container")) {
-        if (itemClass.endsWith(".playlistContainer"))
-            return "album"
-        else
-            return "folder";
-    } else if (itemClass.includes(".audioItem"))
-        return "audiotrack";
-    else if (itemClass.includes(".videoItem"))
-        return "movie";
-    else if (itemClass.includes(".imageItem"))
-        return "insert_photo";
-    else
-        return "insert_drive_file";
-}
+const playerMap: IconMap = {
+    ...classicMap,
+    "container.playlistContainer": "album",
+    "item.audioItem.musicTrack": "audiotrack",
+    "item.videoItem.movie": "movie"
+};
 
 type AlbumArtProps = HTMLAttributes<HTMLOrSVGElement> & { itemClass: string, albumArts?: string[], hint?: "browser" | "player" };
 
 AlbumArt.defaultProps = { itemClass: "object.item" }
 
 export default function AlbumArt({ itemClass, albumArts, className, hint, ...other }: AlbumArtProps) {
-    const getIcon = hint === "player" ? getIconPlayer : getIconClassic;
+    itemClass = itemClass.substring(7);
+    const map = hint === "player" ? playerMap : classicMap;
     return albumArts?.length
         ? <img src={viaProxy(albumArts[0])} className={`album-art${className ? ` ${className}` : ""}`} alt="" {...other} />
         : <svg className={`album-art${className ? ` ${className}` : ""}`} {...other}>
-            <use href={`symbols.svg#${getIcon(itemClass)}`} />
+            <use href={`symbols.svg#${map[itemClass] ?? (itemClass.startsWith("container") ? "folder" : "insert_drive_file")}`} />
         </svg>
 }
