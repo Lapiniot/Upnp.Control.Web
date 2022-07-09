e8885499f447562b579bd2c79f73edddd42400b8
Lapiniot <aossss@gmail.com>
2022-07-09 19:39:59 +0300

    playback control button styles and UI refactored

--- Upnp.Control.Web/ClientApp/src/routes/common/widgets/PlaylistBookmarkWidget.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/widgets/PlaylistBookmarkWidget.tsx
@@ -1,5 +1,4 @@
 import { HTMLAttributes, useCallback } from "react";
-import { MediaQueries } from "../../../components/MediaQueries";
 import { RouteLink } from "../../../components/NavLink";
 import WebApi from "../../../components/WebApi";
 import AlbumArt from "../AlbumArt";
@@ -12,17 +11,17 @@ export type PlaylistBookmarkWidgetProps = HTMLAttributes<HTMLDivElement> & {
     icon: string;
 };
 
+// TODO: fix layout issue with link not occupying available container space
+// TODO: consider merge of album-art and generic icon styles
+
 export default function ({ device, id, title, icon, deviceName }: PlaylistBookmarkWidgetProps) {
     const clickHandler = useCallback(() => WebApi.control(device).playUri(`x-mi://sys/playlist?id=${id.substring(3)}#play`).fetch(), []);
-    const isTouch = MediaQueries.touchDevice.matches;
-    return <div className="hstack overflow-hidden">
-        <div className="playback-aa-ctrl stack ms-2 me-2 flex-shrink-0">
+    return <div className="hstack overflow-hidden align-items-center">
+        <button type="button" className={`btn btn-stack mx-2`}
+            title={`Play \u00AB${title}\u00BB on ${deviceName}`} onClick={clickHandler}>
             <AlbumArt albumArts={icon ? [icon] : undefined} itemClass="object.container.playlistContainer" hint="player" />
-            <button type="button" className={`btn btn-overlay stack-layer${!isTouch ? " stack-layer-hover" : ""}`}
-                title={`Play \u00AB${title}\u00BB on ${deviceName}`} onClick={clickHandler}>
-                <svg className="m-auto icon-lg"><use href="symbols.svg#play_circle" /></svg>
-            </button>
-        </div>
+            <svg className="album-art hover-show fade-in-out text-white bg-black bg-opacity-25"><use href="symbols.svg#play_circle" /></svg>
+        </button>
         <RouteLink to={`/umi/${device}/playlists/${id}`} title={`${title} on ${deviceName}`}
             className="overflow-hidden text-decoration-none ps-0 p-2">
             <h6 className="mb-0 text-truncate">{title}</h6>
--- Upnp.Control.Web/ClientApp/src/routes/umi/playlist/CellTemplate.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/umi/playlist/CellTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { EventHandler, UIEvent } from "react";
+import { EventHandler, UIEvent } from "react";
 import { playlistBookmarks } from "../../../components/BookmarkService";
 import AlbumArt from "../../common/AlbumArt";
 import { useBookmarkButton } from "../../common/BookmarkButton";
@@ -18,31 +18,19 @@ type CellContext = {
 const BookmarkItemButton = useBookmarkButton("PlaylistBookmarkWidget", playlistBookmarks, ["symbols.svg#favorite", "symbols.svg#favorite_border"]);
 
 export default function ({ data: d, context: ctx, index, rowState }: CellTemplateProps<CellContext>) {
+    const active = !!(rowState & RowState.Active);
+    const playing = ctx?.state === "PLAYING";
     return <div className="hstack">
-        <div className="playback-aa-ctrl stack me-2">
+        <button type="button" className="btn btn-stack me-2" data-index={index}
+            onClick={active ? (playing ? ctx.pause : ctx?.play) : ctx?.playItem}>
             <AlbumArt itemClass={d.class} albumArts={d.albumArts} hint="player" />
-            {rowState & RowState.Active
-                ? ctx?.state === "PLAYING"
-                    ? <React.Fragment key="active-playing">
-                        <div className="stack-layer">
-                            <svg className="icon m-auto icon-lg animate-pulse"><use href="symbols.svg#volume_up" /></svg>
-                        </div>
-                        <button type="button" className="btn btn-overlay stack-layer stack-layer-hover" onClick={ctx?.pause}>
-                            <svg className="m-auto icon-lg"><use href="symbols.svg#pause_circle" /></svg>
-                        </button>
-                    </React.Fragment>
-                    : <React.Fragment key="active-paused">
-                        <div className="stack-layer">
-                            <svg className="icon m-auto icon-lg"><use href="symbols.svg#volume_mute" /></svg>
-                        </div>
-                        <button type="button" className="btn btn-overlay stack-layer stack-layer-hover" onClick={ctx?.play}>
-                            <svg className="m-auto icon-lg"><use href="symbols.svg#play_circle" /></svg>
-                        </button>
-                    </React.Fragment>
-                : <button type="button" className="btn btn-overlay stack-layer stack-layer-hover" onClick={ctx?.playItem} data-index={index}>
-                    <svg className="m-auto icon-lg"><use href="symbols.svg#play_circle" /></svg>
-                </button>}
-        </div>
+            {active && <svg className={`album-art hover-hide fade-in-out bg-black bg-opacity-50 ${playing ? "animate-pulse" : "text-white"}`}>
+                <use href={`symbols.svg#${playing ? "volume_up" : "volume_mute"}`} />
+            </svg>}
+            <svg className="album-art hover-show fade-in-out text-white bg-black bg-opacity-25">
+                <use href={`symbols.svg#${active && playing ? "pause_circle" : "play_circle"}`} />
+            </svg>
+        </button>
         <span className="vstack overflow-hidden justify-content-center">
             <span className="text-truncate">{d.title}</span>
             <TrackInfoLine item={d} />
