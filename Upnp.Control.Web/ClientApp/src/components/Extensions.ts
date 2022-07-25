9e1525cb717ce9be3a5575b7563b9ff288578afb
Lapiniot <aossss@gmail.com>
2022-07-25 14:13:27 +0300

    some redundand code removed

--- Upnp.Control.Web/ClientApp/src/components/Extensions.ts
+++ Upnp.Control.Web/ClientApp/src/components/Extensions.ts
@@ -29,31 +29,6 @@ export function viaProxy(url: string) {
     return `/proxy/${encodeURIComponent(url)}`;
 }
 
-/**
- * Finds first element in the DOM tree starting from 'element'
- * which acts as scrolling container (literally has effective values 
- * of overflow-y set to either "auto" or "scroll"). Please notice:
- * this function checks only vertical scroll behavior, as horizontal
- * must be completely abandoned concept in responsive apps.
- * @param {HTMLElement|null} element element to start search from
- * @returns {HTMLElement|null} element which is scrolling container 
- * or null if nothing suitable found
- */
-export function findScrollParent(element: HTMLElement | null): HTMLElement | null {
-    while (element) {
-        if (element.classList.contains("overflow-auto") || element.classList.contains("overflow-scroll"))
-            return element;
-
-        const overflow = window.getComputedStyle(element).getPropertyValue("overflow-y");
-        if (overflow === "auto" || overflow === "scroll")
-            return element;
-
-        element = element.parentElement;
-    }
-
-    return element;
-}
-
 export function nopropagation<E extends SyntheticEvent>(handler: EventHandler<E>): EventHandler<E> {
     return (event: E) => {
         event.stopPropagation();
