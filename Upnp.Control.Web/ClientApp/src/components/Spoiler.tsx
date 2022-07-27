b0d3496be8bdb5d915e48ac4cfdec004f249d5d8
Lapiniot <aossss@gmail.com>
2022-07-27 19:18:38 +0300

    Spoiler component: mandatory uniqueId property removed

--- Upnp.Control.Web/ClientApp/src/components/Spoiler.tsx
+++ Upnp.Control.Web/ClientApp/src/components/Spoiler.tsx
@@ -1,12 +1,22 @@
 ﻿import { HTMLAttributes } from "react";
 import "bootstrap/js/src/collapse";
+import { useId } from "react";
 
-export default ({ uniqueId, title, children, className, ...other }: { uniqueId: string; title: string; } & HTMLAttributes<HTMLDivElement>) =>
-    <div className={`spoiler${className ? ` ${className}` : ""}`} {...other}>
-        <button type="button" className="btn collapsed w-100"
-            data-bs-toggle="collapse" data-bs-target={`#${uniqueId}`}
-            aria-controls={uniqueId} aria-expanded="false">
+type SpoilerProps = HTMLAttributes<HTMLDivElement> & {
+    title: string;
+    disabled?: boolean;
+}
+
+// TODO: investigate for more semantically suitable <details> element
+
+export default ({ title, children, className, disabled, ...other }: SpoilerProps) => {
+    const id = useId().replaceAll(":", "_");
+    return <div className={`spoiler${className ? ` ${className}` : ""}`} {...other}>
+        <button type="button" disabled={disabled} className="btn collapsed w-100"
+            data-bs-toggle="collapse" data-bs-target={`#${id}`}
+            aria-controls={id} aria-expanded="false">
             {title}
         </button>
-        <div className="collapse" id={uniqueId}>{children}</div>
-    </div>;
+        <div className="collapse" id={id}>{children}</div>
+    </div>
+}
--- Upnp.Control.Web/ClientApp/src/routes/common/DeviceServiceList.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/DeviceServiceList.tsx
@@ -1,10 +1,10 @@
 ﻿import Spoiler from "../../components/Spoiler";
 import { DataSourceProps, UpnpService } from "./Types";
 
-export default function ({ "data-source": data, "data-row-id": rowId }: DataSourceProps<UpnpService[]>) {
-    return <Spoiler title="Supported services" uniqueId={`card-${rowId}-s`}>
+export default function ({ "data-source": data }: DataSourceProps<UpnpService[]>) {
+    return <Spoiler title="Supported services">
         <ul className="list-group list-group-flush">
             {[data.map(({ url, usn }, i) => <li key={i} className="list-group-item"><a href={url} className="card-link">{usn}</a></li>)]}
         </ul>
-    </Spoiler>;
+    </Spoiler>
 }
