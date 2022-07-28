52b53bf05ccc6ad0a25e6fc4af767fe19e8bae0b
Lapiniot <aossss@gmail.com>
2022-07-28 15:58:49 +0300

    NavLink & Link components: render disabled placeholder if to attribute is undefined

--- Upnp.Control.Web/ClientApp/src/components/NavLink.tsx
+++ Upnp.Control.Web/ClientApp/src/components/NavLink.tsx
@@ -3,26 +3,26 @@ import { NavLink, NavLinkProps } from "react-router-dom";
 import { useNavigatorClickHandler, useNavigatorResolvedPath } from "./Navigator";
 
 export type LinkProps = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "href"> & {
-    to: string;
+    to: string | undefined;
     disabled?: boolean;
     glyph?: string
 }
 
 export function Link({ to, glyph, className, disabled, children, ...other }: LinkProps) {
     const content = <>{glyph && <svg><use href={glyph} /></svg>}{children}</>;
-    return !disabled
+    return !disabled && to
         ? <a href={to} className={className} {...other}>{content}</a>
         : <span className={`link-placeholder disabled${className ? ` ${className}` : ""}`}>{content}</span>
 }
 
-export function RouteLink({ glyph, className, disabled, children, to, ...other }: NavLinkProps & LinkProps) {
+export function RouteLink({ glyph, className, disabled, children, to, ...other }: Omit<NavLinkProps, "to"> & LinkProps) {
     const content = <>{glyph && <svg><use href={glyph} /></svg>}{children}</>;
-    return !disabled
+    return !disabled && to
         ? <NavLink to={to} className={className} {...other}>{content}</NavLink>
         : <span className={`link-placeholder disabled${className ? ` ${className}` : ""}`}>{content}</span>
 }
 
-export function NavigatorLink({ to, ...other }: LinkProps) {
+export function NavigatorLink({ to = "#", ...other }: LinkProps) {
     const { pathname, search, hash } = useNavigatorResolvedPath(to);
     const handler = useNavigatorClickHandler();
     return <Link {...other} to={pathname + hash + search} onClick={handler} />
