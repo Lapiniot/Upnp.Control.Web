d55a27431c445d2b781a6fff13748ec4a2d1c4b0
Lapiniot <aossss@gmail.com>
2022-08-15 19:04:59 +0300

    setupProxy.js: fixed proxy crashes

--- Upnp.Control.Web/ClientApp/src/setupProxy.js
+++ Upnp.Control.Web/ClientApp/src/setupProxy.js
@@ -1,28 +1,21 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const { env } = require('process');
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const { env } = require('process')
 
 const target = env.ASPNETCORE_HTTPS_PORT
-	? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
-	: env.ASPNETCORE_URLS
-		? env.ASPNETCORE_URLS.split(';')[0]
-		: 'http://localhost:8080';
+    ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+    : env.ASPNETCORE_URLS
+        ? env.ASPNETCORE_URLS.split(';')[0]
+        : 'http://localhost:8080'
 
-const context = [
-	"/api",
-	"/proxy",
-	"/dlna-proxy"
-];
+const options = {
+    target,
+    secure: false,
+    onError: (error) => console.error(error.message)
+}
 
 module.exports = function (app) {
-
-	app.use(createProxyMiddleware(context, {
-		target,
-		secure: false
-	}));
-
-	app.use(createProxyMiddleware("/upnpevents", {
-		target,
-		secure: false,
-		ws: true
-	}));
-};
+    app.use(createProxyMiddleware(["/api", "/proxy", "/dlna-proxy"],
+        { ...options, headers: { Connection: 'Keep-Alive' } }));
+    app.use(createProxyMiddleware("/upnpevents",
+        { ...options, ws: true }));
+}
