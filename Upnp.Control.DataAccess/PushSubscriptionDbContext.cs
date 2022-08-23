ccb614514e927fc41d8f0b188f1534f62d445308
Lapiniot <aossss@gmail.com>
2022-08-23 15:37:26 +0300

    Upnp.Control.Models.* made trimmable

--- Upnp.Control.DataAccess/PushSubscriptionDbContext.cs
+++ Upnp.Control.DataAccess/PushSubscriptionDbContext.cs
@@ -5,9 +5,12 @@ namespace Upnp.Control.DataAccess;
 
 internal sealed class PushSubscriptionDbContext : DbContext
 {
+    [DynamicDependency(PublicConstructors, typeof(PushNotificationSubscription))]
+    private DbSet<PushNotificationSubscription> subscriptions;
+
     public PushSubscriptionDbContext(DbContextOptions<PushSubscriptionDbContext> options) : base(options) { }
 
-    public DbSet<PushNotificationSubscription> Subscriptions { get; set; }
+    public DbSet<PushNotificationSubscription> Subscriptions { get => subscriptions; set => subscriptions = value; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfiguration(new PushSubscriptionEntityType());
 }
--- Upnp.Control.DataAccess/UpnpDbContext.cs
+++ Upnp.Control.DataAccess/UpnpDbContext.cs
@@ -4,9 +4,14 @@ namespace Upnp.Control.DataAccess;
 
 internal sealed class UpnpDbContext : DbContext
 {
+    [DynamicDependency(PublicConstructors, typeof(UpnpDbContext))]
+    [DynamicDependency(PublicConstructors, typeof(Service))]
+    [DynamicDependency(PublicConstructors, typeof(Icon))]
+    private DbSet<UpnpDevice> upnpDevices;
+
     public UpnpDbContext(DbContextOptions<UpnpDbContext> options) : base(options) { }
 
-    public DbSet<UpnpDevice> UpnpDevices { get; set; }
+    public DbSet<UpnpDevice> UpnpDevices { get => upnpDevices; set => upnpDevices = value; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfiguration(new DeviceEntityType());
 }
