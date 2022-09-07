f0eb0f92f595834c8bf4519de66e6a33e6489874
Lapiniot <aossss@gmail.com>
2022-09-07 20:55:25 +0300

    FIX: NotificationHost state is not updated correctly

--- Upnp.Control.Web/ClientApp/src/components/NotificationsHost.tsx
+++ Upnp.Control.Web/ClientApp/src/components/NotificationsHost.tsx
@@ -14,25 +14,23 @@ export interface INotificationHost {
     push(notification: Notification): void
 }
 
-function initializer() {
-    return new Map<string, Notification>()
-}
+function initializer() { return { map: new Map<string, Notification>() } }
 
 export default function (_: unknown, ref: React.Ref<INotificationHost>) {
-    const [state, setState] = React.useState(initializer);
+    const [{ map }, setState] = React.useState(initializer);
 
     const handler = React.useCallback(({ dataset: { id: key } }: HTMLDivElement) => {
-        setState(state => { state.delete(key!); return state })
+        setState(({ map }) => { map.delete(key!); return { map } })
     }, []);
 
     React.useImperativeHandle(ref, () => ({
         push(notification) {
-            setState(state => { state.set(notification.id, notification); return state; })
+            setState(({ map }) => { map.set(notification.id, notification); return { map }; })
         }
     }), []);
 
     return <Portal selector="#notifications-root">
-        {Array.from(state).map(({ 0: k, 1: { title, color, delay, message } }) =>
+        {Array.from(map).map(({ 0: k, 1: { title, color, delay, message } }) =>
             <Toast key={k} data-id={k} header={title} color={color} autohide={!!(delay && delay > 0)} delay={delay} onDismissed={handler}>{message}</Toast>)}
     </Portal>
 }
