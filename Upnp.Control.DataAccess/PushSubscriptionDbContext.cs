4194a68cf9a474192493abd2c61e4304529288b1
Lapiniot <aossss@gmail.com>
2022-10-13 20:30:19 +0300

    Analyzer warnings suppressed

--- Upnp.Control.DataAccess/PushSubscriptionDbContext.cs
+++ Upnp.Control.DataAccess/PushSubscriptionDbContext.cs
@@ -6,6 +6,7 @@ namespace Upnp.Control.DataAccess;
 internal sealed class PushSubscriptionDbContext : DbContext
 {
     [DynamicDependency(PublicConstructors, typeof(PushNotificationSubscription))]
+    [UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2026:RequiresUnreferencedCode", Justification = "Preserved manually.")]
     public PushSubscriptionDbContext(DbContextOptions<PushSubscriptionDbContext> options) : base(options) { }
 
     public DbSet<PushNotificationSubscription> Subscriptions { get; set; }
--- Upnp.Control.DataAccess/UpnpDbContext.cs
+++ Upnp.Control.DataAccess/UpnpDbContext.cs
@@ -7,6 +7,7 @@ internal sealed class UpnpDbContext : DbContext
     [DynamicDependency(PublicConstructors, typeof(UpnpDbContext))]
     [DynamicDependency(PublicConstructors, typeof(Service))]
     [DynamicDependency(PublicConstructors, typeof(Icon))]
+    [UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2026:RequiresUnreferencedCode", Justification = "Preserved manually.")]
     public UpnpDbContext(DbContextOptions<UpnpDbContext> options) : base(options) { }
 
     public DbSet<UpnpDevice> UpnpDevices { get; set; }
