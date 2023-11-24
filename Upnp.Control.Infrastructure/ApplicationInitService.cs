da5cc4cd46a19a6ef484394c8aa8fe92b76990dc
Lapiniot <aossss@gmail.com>
2023-11-24 18:25:48 +0200

    make ApplicationInitService implement IHostedLifecycleService to ensure initializers run at the very beggining

--- Upnp.Control.Infrastructure/ApplicationInitService.cs
+++ Upnp.Control.Infrastructure/ApplicationInitService.cs
@@ -2,25 +2,13 @@ using Upnp.Control.Abstractions;
 
 namespace Upnp.Control.Infrastructure;
 
-internal sealed partial class ApplicationInitService(IServiceProvider services, ILogger<ApplicationInitService> logger) : IHostedService
+internal sealed partial class ApplicationInitService(
+    IServiceProvider services, ILogger<ApplicationInitService> logger) :
+    IHostedService, IHostedLifecycleService
 {
     private readonly ILogger<ApplicationInitService> logger = logger;
 
-    public async Task StartAsync(CancellationToken cancellationToken)
-    {
-        LogStarted();
-
-        using var scope = services.CreateScope();
-
-        await Task.WhenAll(scope.ServiceProvider.GetServices<IServiceInitializer>()
-            .Select(initializer =>
-            {
-                LogStartInit(initializer.GetType().Name);
-                return initializer.InitializeAsync(cancellationToken);
-            })).ConfigureAwait(false);
-
-        LogDone();
-    }
+    public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
@@ -32,4 +20,22 @@ internal sealed partial class ApplicationInitService(IServiceProvider services, 
 
     [LoggerMessage(3, LogLevel.Information, "Initialization is done")]
     private partial void LogDone();
+
+    public async Task StartingAsync(CancellationToken cancellationToken)
+    {
+        LogStarted();
+        using var scope = services.CreateScope();
+        var initializers = scope.ServiceProvider.GetServices<IServiceInitializer>();
+        foreach (var initializer in initializers)
+        {
+            LogStartInit(initializer.GetType().Name);
+            await initializer.InitializeAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        LogDone();
+    }
+
+    public Task StartedAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    public Task StoppingAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    public Task StoppedAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 }
