9fe6c533f923f32a51eb6142c01262e26390401b
Lapiniot <aossss@gmail.com>
2023-12-11 14:49:13 +0200

    vite-plugin-generate-service-worker implemented

---
+++ Upnp.Control.Web/ClientApp/vite-plugin-generate-service-worker.ts
@@ -0,0 +1,49 @@
+import { RollupOptions, rollup } from "rollup"
+import { Plugin } from "vite"
+
+type Partial2<T> = {
+    [P in keyof T]?:
+    T[P] extends {} ? Partial<T[P]> :
+    T[P];
+};
+
+type GenerateServiceWorkerOptions = {
+    swFileName: string,
+    rollupOptions: RollupOptions,
+    precache: {
+        filter(assetName: string): boolean,
+        manifestPlaceholder: string,
+        manifestExtraFiles: string[]
+    }
+}
+
+const defaults: GenerateServiceWorkerOptions = {
+    swFileName: "service-worker.js",
+    rollupOptions: {
+        input: {
+            "service-worker": "./src/service-worker.js"
+        }
+    },
+    precache: {
+        filter: (name) => name !== "service-worker.js",
+        manifestPlaceholder: "self.__WB_MANIFEST",
+        manifestExtraFiles: []
+    }
+}
+
+export default function generateSW(options: Partial2<GenerateServiceWorkerOptions> = {}): Plugin {
+    const { swFileName, rollupOptions, precache: { filter, manifestExtraFiles, manifestPlaceholder } } =
+        { ...defaults, ...options, precache: { ...defaults.precache, ...options.precache } };
+    return {
+        name: "vite-plugin-generate-service-worker",
+        async generateBundle(_, bundle) {
+            const names = Object.getOwnPropertyNames(bundle).concat(manifestExtraFiles).filter(filter);
+            const build = await rollup(rollupOptions);
+            const { output: [{ code }] } = await build.generate({});
+            this.emitFile({
+                type: "asset", name: "service-worker", fileName: swFileName,
+                source: code.replaceAll(manifestPlaceholder, JSON.stringify(names))
+            })
+        }
+    }
+}
--- Upnp.Control.Web/ClientApp/vite.config.ts
+++ Upnp.Control.Web/ClientApp/vite.config.ts
@@ -1,11 +1,26 @@
-import { defineConfig, loadEnv } from "vite"
+import resolve from "@rollup/plugin-node-resolve"
 import react from "@vitejs/plugin-react"
 import fs from "fs"
+import typescript from "rollup-plugin-typescript"
+import { defineConfig, loadEnv } from "vite"
+import generateSW from "./vite-plugin-generate-service-worker"
 
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => {
-    if (mode !== "development") return { plugins: [react()] };
+    if (mode !== "development") return {
+        plugins: [react(), generateSW({
+            precache: {
+                manifestExtraFiles: ["stack.svg", "symbols.svg", "icons/icon.svg", "icons/favicon.ico", "icons/apple-touch-icon.png",
+                    "icons/48.png", "icons/72.png", "icons/96.png", "icons/128.png", "icons/144.png", "icons/152.png", "icons/192.png",
+                    "icons/384.png", "icons/512.png", "manifest.webmanifest"]
+            },
+            rollupOptions: {
+                input: { "service-worker": "./src/service-worker.ts" },
+                plugins: [typescript(), resolve()],
+            }
+        })]
+    };
 
     const env = loadEnv(mode, process.cwd(), '');
     const options = { target: env.PROXY_TO ?? "http://localhost:8080" }
