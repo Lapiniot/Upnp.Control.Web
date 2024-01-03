19b24727a327a63ae3147e9e80cc59d9a99d9abc
Lapiniot <aossss@gmail.com>
2024-01-03 16:48:49 +0200

    Replace html <img> elements with <svg> for generic device icons in order to inherit theme colors automatically

--- Upnp.Control.Web/ClientApp/src/routes/common/DeviceIcon.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/DeviceIcon.tsx
@@ -1,14 +1,17 @@
-import { ImgHTMLAttributes } from "react";
+import { ImgHTMLAttributes, SVGProps } from "react";
 import { viaProxy } from "../../services/Extensions";
 import { UpnpDeviceTools as UDT } from "./UpnpDeviceTools";
 
-type DeviceIconProps = ImgHTMLAttributes<HTMLImageElement> & {
-    device: Upnp.Device | null | undefined;
-}
+type CommonKeys<K, V> = { [P in keyof K & keyof V]: K[P] extends V[P] ? V[P] extends K[P] ? P : never : never }[keyof K & keyof V]
+type Common<K, V> = { [P in CommonKeys<K, V>]: K[P] }
+type ImgOrSvgCommonProps = Partial<Common<ImgHTMLAttributes<HTMLImageElement>, SVGProps<SVGElement>>>
+type DeviceIconProps = ImgOrSvgCommonProps & { device: Upnp.Device | null | undefined }
 
 export default ({ device, className, ...props }: DeviceIconProps) => {
     const { icons, type } = device ?? {};
     const icon = icons && UDT.getOptimalIcon(icons);
-    return <img src={icons && (icon ? viaProxy(icon.url) : `stack.svg#${UDT.getSpecialRoleIcon(type!)}`)}
-        className={`icon icon-3x me-2${className ? ` ${className}` : ""}`} alt="" {...props} />
+    const iconClass = `icon icon-3x me-2${className ? ` ${className}` : ""}`;
+    return icon
+        ? <img src={viaProxy(icon.url)} className={iconClass} alt="" {...props} />
+        : <svg className={iconClass} {...props}><use href={`stack.svg#${UDT.getSpecialRoleIcon(type!)}`} /></svg>
 }
--- Upnp.Control.Web/ClientApp/src/routes/common/widgets/DeviceBookmarkWidget.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/widgets/DeviceBookmarkWidget.tsx
@@ -13,7 +13,9 @@ type DeviceBookmarkWidgetProps = HTMLAttributes<HTMLDivElement> & {
 export default function ({ category, device, name, description, icon }: DeviceBookmarkWidgetProps) {
     const iconUrl = icon.startsWith("http") ? viaProxy(icon) : icon;
     return <RouteLink to={`/${category}/${device}`} className="hstack flex-fill min-w-0 text-decoration-none p-2">
-        <img src={iconUrl} className="icon-2x me-2 flex-shrink-0" alt="" />
+        {iconUrl.includes(".svg")
+            ? <svg className="icon-2x me-2"><use href={iconUrl} /></svg>
+            : <img src={iconUrl} className="icon-2x me-2" alt="" />}
         <div className="min-w-0">
             <h5 className="mb-0 text-truncate">{name}</h5>
             <p className="mb-0 text-truncate">{description}</p>
