04dca2e1c826dbfd6e20a4eb0e60ff887e673be2
Lapiniot <aossss@gmail.com>
2024-01-17 22:06:30 +0200

    SsdpSearchEnumerator socket configuration handling improved

--- Upnp.Control.Infrastructure.UpnpDiscovery/Configuration/ConfigureServicesExtensions.cs
+++ Upnp.Control.Infrastructure.UpnpDiscovery/Configuration/ConfigureServicesExtensions.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using IoT.Protocol.Upnp;
 using OOs.Net.Sockets;
 using OOs.Policies;
@@ -7,7 +9,7 @@ using static OOs.Net.NetworkInterfaceExtensions;
 
 namespace Upnp.Control.Infrastructure.UpnpDiscovery.Configuration;
 
-public static class ConfigureServicesExtensions
+public static partial class ConfigureServicesExtensions
 {
     public static IServiceCollection AddUpnpDiscovery(this IServiceCollection services) =>
         services
@@ -28,23 +30,36 @@ public static class ConfigureServicesExtensions
     private static SsdpSearchEnumerator SsdpEnumeratorFactory(IServiceProvider serviceProvider)
     {
         var options = serviceProvider.GetRequiredService<IOptions<SsdpOptions>>().Value;
+        var logger = serviceProvider.GetRequiredService<ILogger<UpnpDiscoveryService>>();
 
         return new SsdpSearchEnumerator(UpnpServices.RootDevice,
-            (socket, ep) => socket
-                .ConfigureMulticast(GetInterfaceIndex(options.MulticastInterface), options.MulticastTTL)
-                .JoinMulticastGroup(ep),
+            (socket, groupEndPoint) =>
+            {
+                var mcintAddress = (options.MulticastInterface switch
+                {
+                    "auto" => FindBestMulticastInterface(),
+                    "any" => null,
+                    var name => FindInterface(name)
+                })?.GetPrimaryAddress(socket.AddressFamily);
+
+                socket.ConfigureMulticastOptions(mcintAddress, options.MulticastTTL).JoinMulticastGroup(groupEndPoint, mcintAddress);
+
+                // Query MulticastInterface option from the configured socket directly to get effectively applied value
+                var mcint = (int)socket.GetSocketOption(socket.AddressFamily is AddressFamily.InterNetwork ? SocketOptionLevel.IP : SocketOptionLevel.IPv6, SocketOptionName.MulticastInterface);
+                LogMulticastConfiguration(logger, groupEndPoint, new IPAddress(mcint));
+            },
             new RepeatPolicyBuilder()
                 .WithExponentialInterval(2, options.SearchIntervalSeconds)
                 .WithJitter(500, 1000)
                 .Build());
     }
 
-    private static int GetInterfaceIndex(string nameOrIdOrAddress)
+    private static NetworkInterface FindInterface(string nameOrIdOrAddress)
     {
-        var mcastInterface = (nameOrIdOrAddress is "any" or "auto"
-            ? FindBestMulticastInterface()
-            : FindByName(nameOrIdOrAddress) ?? FindByAddress(nameOrIdOrAddress) ?? FindById(nameOrIdOrAddress))
-            ?? throw new ArgumentException("Requested interface was not found");
+        var mcastInterface = FindByName(nameOrIdOrAddress) ??
+            FindByAddress(nameOrIdOrAddress) ??
+            FindById(nameOrIdOrAddress) ??
+            throw new ArgumentException("Requested interface was not found");
 
         if (!mcastInterface.SupportsMulticast)
         {
@@ -56,6 +71,9 @@ public static class ConfigureServicesExtensions
             throw new ArgumentException("Requested interface is not in operational state");
         }
 
-        return mcastInterface.GetIPProperties().GetIPv4Properties().Index;
+        return mcastInterface;
     }
+
+    [LoggerMessage(LogLevel.Information, "Joined '{groupEndPoint}' multicast group on '{mcintAddress}' interface for SSDP discovery")]
+    private static partial void LogMulticastConfiguration(ILogger logger, IPEndPoint groupEndPoint, IPAddress mcintAddress);
 }
