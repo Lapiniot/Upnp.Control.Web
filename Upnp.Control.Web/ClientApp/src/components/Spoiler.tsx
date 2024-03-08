d08b2669633780140cae3898f67921d0b3f9b60a
Lapiniot <aossss@gmail.com>
2024-03-08 22:49:55 +0200

    Spoiler components animation added

--- Upnp.Control.Web/ClientApp/src/components/Spoiler.tsx
+++ Upnp.Control.Web/ClientApp/src/components/Spoiler.tsx
@@ -1,13 +1,23 @@
-﻿import { DetailsHTMLAttributes } from "react";
+import { DetailsHTMLAttributes } from "react";
 
 type SpoilerProps = DetailsHTMLAttributes<HTMLDetailsElement> & {
-    title: string;
+    caption: string;
     disabled?: boolean;
 }
 
-export default ({ title, children, className, disabled, ...other }: SpoilerProps) => {
-    return <details className={`spoiler${className ? ` ${className}` : ""}`} {...other} inert={disabled ? "" : undefined}>
-        <summary>{title}</summary>
-        <div>{children}</div>
+export default ({ caption, children, className, disabled, open, ...other }: SpoilerProps) => {
+    return <details open className={`spoiler${className ? ` ${className}` : ""}`} {...other} inert={disabled ? "" : undefined}>
+        <summary>
+            <input type="checkbox" />
+            <span>{caption}</span>
+        </summary>
+        <div>
+            {/* Do not remove this wrapper div! It wraps children content and has to have overflow: hidden set.
+            It makes the trick together with parent div with display: grid and dynamic content 
+            row height animatable from 0fr to 1fr. */}
+            <div>
+                {children}
+            </div>
+        </div>
     </details>
 }
--- Upnp.Control.Web/ClientApp/src/routes/common/DeviceServiceList.tsx
+++ Upnp.Control.Web/ClientApp/src/routes/common/DeviceServiceList.tsx
@@ -1,7 +1,7 @@
 ﻿import Spoiler from "../../components/Spoiler";
 
 export default function ({ dataSource: services }: DataSourceProps<Upnp.Service[]>) {
-    return <Spoiler title="Supported services" disabled={!services}>
+    return <Spoiler caption="Supported services" disabled={!services}>
         <ul className="list-group list-group-flush">
             {[services?.map(({ url, usn }, i) => <li key={i} className="list-group-item"><a href={url} className="card-link">{usn}</a></li>)]}
         </ul>
