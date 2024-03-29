77194531fefc4f36996b3204c5ee846cb0d73659
Lapiniot <aossss@gmail.com>
2024-03-29 16:01:23 +0200

    SlideGestureRecognizer: use passive pointer-events handlers

--- Upnp.Control.Web/ClientApp/src/services/gestures/SlideGestureRecognizer.ts
+++ Upnp.Control.Web/ClientApp/src/services/gestures/SlideGestureRecognizer.ts
@@ -13,15 +13,13 @@ export class SlideGestureRecognizer<TElement extends HTMLElement = HTMLElement> 
     y: number = 0;
 
     constructor(handler: GestureHandler<TElement, "slide", SlideParams>, capture = false) {
-        super(handler, capture, false);
+        super(handler, capture, true);
     }
 
     protected override onPointerDown(event: PointerEvent) {
         const { target, clientX, clientY, pointerId } = event;
         const element = target as TElement;
         if (this.handler(element, "slide", { phase: "start", x: clientX, y: clientY }) !== false) {
-            event.stopPropagation();
-            event.preventDefault();
             element.setPointerCapture(pointerId);
             super.onPointerDown(event);
         }
@@ -30,8 +28,6 @@ export class SlideGestureRecognizer<TElement extends HTMLElement = HTMLElement> 
     protected override onPointerUp(event: PointerEvent) {
         const { target, clientX, clientY, pointerId } = event;
         const element = target as TElement;
-        event.preventDefault();
-        event.stopPropagation();
         element.releasePointerCapture(pointerId);
         super.onPointerUp(event);
         requestAnimationFrame(() => this.handler(element, "slide", { phase: "end", x: clientX, y: clientY }));
@@ -42,8 +38,6 @@ export class SlideGestureRecognizer<TElement extends HTMLElement = HTMLElement> 
     }
 
     protected override onPointerMoveEvent(event: PointerEvent) {
-        event.stopPropagation();
-        event.preventDefault();
         super.onPointerMoveEvent(event);
         this.x = event.clientX;
         this.y = event.clientY;
