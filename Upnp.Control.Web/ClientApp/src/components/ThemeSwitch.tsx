3d193461b7aaebebe8f969d3897b41e63c7a2e78
Lapiniot <aossss@gmail.com>
2024-04-17 19:43:09 +0300

    ThemeSwitch responsive mode reworked

--- Upnp.Control.Web/ClientApp/src/components/ThemeSwitch.tsx
+++ Upnp.Control.Web/ClientApp/src/components/ThemeSwitch.tsx
@@ -1,5 +1,3 @@
-import { useMediaQuery } from "../hooks/MediaQuery";
-import { MediaQueries } from "../services/MediaQueries";
 import { ThemeContext } from "./ThemeContext";
 import { HTMLAttributes, useCallback, useContext } from "react";
 
@@ -12,25 +10,21 @@ const sequence: { [K in UI.Theme]: number } = { "light": 1, "dark": 2, "auto": 0
 
 export function ThemeSwitch({ mode = "compact", className }: HTMLAttributes<HTMLDivElement> & ThemeSwitchProps) {
     const [current, setTheme] = useContext(ThemeContext);
-    const mediumScreen = useMediaQuery(MediaQueries.mediumScreen, mode === "responsive");
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const toggleTheme = useCallback((event: React.MouseEvent<HTMLButtonElement>) => setTheme(event.currentTarget.dataset["themeToggle"] as UI.Theme), []);
     const next = themes[sequence[current]];
-    const renderMode: typeof mode = mode === "responsive"
-        ? mediumScreen ? "compact" : "icon"
-        : mode;
-    return <div className={`btn-group theme-switch${className ? ` ${className}` : ""}`}>
-        {renderMode === "icon"
-            ? <button data-theme-toggle={next[0]} title={`Switch to ${next[0]} theme.`} onClick={toggleTheme}
+    return <div className={`${mode === "responsive" ? "theme-switch-responsive" : ""}${className ? ` ${className}` : ""}`}>
+        {(mode === "icon" || mode === "responsive") &&
+            <button data-theme-toggle={next[0]} title={`Switch to ${next[0]} theme.`} onClick={toggleTheme}
                 className="btn btn-icon btn-outline">
                 <svg><use href={`symbols.svg#${next[1]}`}></use></svg>
-            </button>
-            : themes.map(([name, icon]) =>
-                <button data-theme-toggle={name} key={name} title={`Switch to ${name} theme.`} onClick={toggleTheme}
-                    className={`btn btn-outline${current == name ? " active" : ""}${mode !== "full" ? " btn-icon" : ""}`}>
-                    <svg><use href={`symbols.svg#${icon}`}></use></svg>
-                    {mode === "full" && <span className="text-capitalize">{name}</span>}
-                </button>)
-        }
+            </button>}
+        {mode !== "icon" && <div className="btn-group w-100">{themes.map(([name, icon]) =>
+            <button data-theme-toggle={name} key={name} title={`Switch to ${name} theme.`} onClick={toggleTheme}
+                className={`btn btn-outline${current == name ? " active" : ""}${mode === "compact" ? " btn-icon" : ""}`}>
+                <svg><use href={`symbols.svg#${icon}`}></use></svg>
+                {(mode === "full" || mode === "responsive") && <span className="text-capitalize">{name}</span>}
+            </button>)}
+        </div>}
     </div>
 }
