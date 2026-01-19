58e4170594e54cb86dc7aa2995d54424db933ba0
Lapiniot <aossss@gmail.com>
2026-01-19 23:21:53 +0200

    1. Added .net10 TFM to supported frameworks list 2. Deprecated .net8

--- Upnp.Control.Infrastructure.AspNetCore/CertificateLoader.cs
+++ Upnp.Control.Infrastructure.AspNetCore/CertificateLoader.cs
@@ -7,11 +7,7 @@ internal static class CertificateLoader
 {
     public static X509Certificate2 LoadFromFile(string path, string? password) =>
         File.Exists(path)
-#if NET9_0_OR_GREATER
-                ? X509CertificateLoader.LoadPkcs12FromFile(path, password)
-#else
-                ? new X509Certificate2(path, password)
-#endif
+            ? X509CertificateLoader.LoadPkcs12FromFile(path, password)
             : throw new InvalidOperationException("File doesn't exist at requested path");
 
     public static X509Certificate2 LoadFromStore(string storeName, string storeLocation, string subject, bool allowInvalid) =>
